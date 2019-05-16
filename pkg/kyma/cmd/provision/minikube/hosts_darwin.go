0eb027821731a7751837a9fc5ff6a1eae27ce20f
igor-karpukhin <igor.karpukhin@sap.com>
2019-05-16 08:48:16 +0200

    Added user question if it's running CLI runs with sudo

--- pkg/kyma/cmd/provision/minikube/hosts_darwin.go
+++ pkg/kyma/cmd/provision/minikube/hosts_darwin.go
@@ -3,6 +3,11 @@
 package minikube
 
 import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+
 	"github.com/kyma-project/cli/internal"
 	"github.com/kyma-project/cli/internal/step"
 )
@@ -11,18 +16,51 @@ const hostsFile = "/etc/hosts"
 
 const defaultVMDriver = vmDriverHyperkit
 
-func addDevDomainsToEtcHostsOSSpecific(o *MinikubeOptions, s step.Step, hostAlias string) error {
-	s.LogInfo("Please enter your password if requested")
+func isWithSudo() bool {
+	val := os.Getenv("SUDO_UID")
+	if val != "" {
+		return true
+	}
+	return false
+}
 
+func addDevDomainsToEtcHostsOSSpecific(o *MinikubeOptions, s step.Step, hostAlias string) error {
+	s.LogInfo("Adding aliases to your 'hosts' file")
 	_, err := internal.RunCmd("sudo", []string{"/bin/sh", "-c", "sed -i '' \"/" + o.Domain + "/d\" " + hostsFile})
 	if err != nil {
 		return err
 	}
-
-	_, err = internal.RunCmd("sudo", []string{"/bin/sh", "-c", "echo '" + hostAlias + "' >> " + hostsFile})
-	if err != nil {
-		return err
+	if isWithSudo() {
+		s.LogInfo("You're running CLI with sudo. CLI has to add dns mapping to your 'hosts'. Type 'yes' to allow this action")
+		for {
+			fmt.Print("Type 'yes' or 'no': ")
+			var res string
+			_, err := fmt.Scanf("%s", &res)
+			if err != nil {
+				continue
+			}
+			switch res {
+			case "yes":
+				cmd := exec.Command("sudo", "tee", "-a", hostsFile)
+				buf := &bytes.Buffer{}
+				_, err = fmt.Fprint(buf, hostAlias)
+				if err != nil {
+					return err
+				}
+				cmd.Stdin = buf
+				err = cmd.Run()
+				if err != nil {
+					return err
+				}
+				return nil
+			case "no":
+				break
+			default:
+				continue
+			}
+		}
 	}
+	fmt.Printf("Execute the following command manually to add mappings: echo '%s' | tee -a /etc/hosts\r\n", hostAlias)
 
 	return nil
 }
