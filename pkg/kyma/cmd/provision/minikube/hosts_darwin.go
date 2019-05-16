69391bd915dda9467b24402603b857a6bc86ded5
igor-karpukhin <igor.karpukhin@sap.com>
2019-05-16 13:14:43 +0200

    Refactored editing the hosts file

--- pkg/kyma/cmd/provision/minikube/hosts_darwin.go
+++ pkg/kyma/cmd/provision/minikube/hosts_darwin.go
@@ -24,43 +24,57 @@ func isWithSudo() bool {
 	return false
 }
 
-func addDevDomainsToEtcHostsOSSpecific(o *MinikubeOptions, s step.Step, hostAlias string) error {
-	s.LogInfo("Adding aliases to your 'hosts' file")
-	_, err := internal.RunCmd("sudo", []string{"/bin/sh", "-c", "sed -i '' \"/" + o.Domain + "/d\" " + hostsFile})
-	if err != nil {
-		return err
-	}
-	if isWithSudo() {
-		s.LogInfo("You're running CLI with sudo. CLI has to add dns mapping to your 'hosts'. Type 'yes' to allow this action")
-		for {
-			fmt.Print("Type 'yes' or 'no': ")
-			var res string
-			_, err := fmt.Scanf("%s", &res)
-			if err != nil {
-				continue
-			}
-			switch res {
-			case "yes":
-				cmd := exec.Command("sudo", "tee", "-a", hostsFile)
-				buf := &bytes.Buffer{}
-				_, err = fmt.Fprint(buf, hostAlias)
-				if err != nil {
-					return err
-				}
-				cmd.Stdin = buf
-				err = cmd.Run()
-				if err != nil {
-					return err
-				}
-				return nil
-			case "no":
-				break
-			default:
-				continue
-			}
+func promptUser() bool {
+	for {
+		fmt.Print("Type [y/n]: ")
+		var res string
+		_, err := fmt.Scanf("%s", &res)
+		if err != nil {
+			return false
+		}
+		switch res {
+		case "yes":
+			return true
+		case "no":
+			return false
+		default:
+			continue
 		}
 	}
-	fmt.Printf("Execute the following command manually to add mappings: echo '%s' | tee -a /etc/hosts\r\n", hostAlias)
+}
 
+func addDevDomainsToEtcHostsOSSpecific(o *MinikubeOptions, s step.Step, hostAlias string) error {
+	notifyUserFunc := func() {
+		s.LogInfof("Execute the following command manually to add mappings: sudo sed -i '' \"/"+o.Domain+"/d\" "+hostsFile+" && echo '%s' | sudo tee -a /etc/hosts\r\n", hostAlias)
+		return
+	}
+
+	s.LogInfo("Adding aliases to your 'hosts' file")
+	if isWithSudo() {
+		s.LogInfo("You're running CLI with sudo. CLI has to add Kyma domain entries to your 'hosts'. Type 'y' to allow this action")
+		if !promptUser() {
+			notifyUserFunc()
+			return nil
+		}
+	}
+	_, err := internal.RunCmd("sudo", []string{"/bin/sh", "-c", "sed -i '' \"/" + o.Domain + "/d\" " + hostsFile})
+	if err != nil {
+		notifyUserFunc()
+		return nil
+	}
+
+	cmd := exec.Command("sudo", "tee", "-a", hostsFile)
+	buf := &bytes.Buffer{}
+	_, err = fmt.Fprint(buf, hostAlias)
+	if err != nil {
+		notifyUserFunc()
+		return nil
+	}
+	cmd.Stdin = buf
+	err = cmd.Run()
+	if err != nil {
+		notifyUserFunc()
+		return nil
+	}
 	return nil
 }
