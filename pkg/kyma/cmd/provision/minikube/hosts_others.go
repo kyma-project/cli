c904a700322aac235e889dedd35bb82f1c9d32cc
Igor <igor.karpukhin@sap.com>
2019-05-20 10:18:42 +0200

    Refactored editing /etc/hosts file (#75)
    * Added user question if it's running CLI runs with sudo
    
    * Refactored editing the hosts file
    
    * Fixes according to the review
    
    * Unified the way of updating hosts file for OSX and *nix

--- pkg/kyma/cmd/provision/minikube/hosts_others.go
+++ pkg/kyma/cmd/provision/minikube/hosts_others.go
@@ -3,27 +3,6 @@
 
 package minikube
 
-import (
-	"github.com/kyma-project/cli/internal"
-	"github.com/kyma-project/cli/internal/step"
-)
-
 const hostsFile = "/etc/hosts"
 
 const defaultVMDriver = vmDriverNone
-
-func addDevDomainsToEtcHostsOSSpecific(o *MinikubeOptions, s step.Step, hostAlias string) error {
-	s.LogInfo("Please enter your password if requested")
-
-	_, err := internal.RunCmd("sudo", []string{"/bin/sh", "-c", "sed -i '' \"/" + o.Domain + "/d\" " + hostsFile})
-	if err != nil {
-		return err
-	}
-
-	_, err = internal.RunCmd("sudo", []string{"/bin/sh", "-c", "echo '" + hostAlias + "' >> " + hostsFile})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
