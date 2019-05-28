9b9d3c924c80bc8a89618a34163c4928be2291e7
Atakan Yenel <atakan.yenel@sap.com>
2019-05-28 15:21:06 +0200

    fix hosts lines on windows

--- pkg/kyma/cmd/provision/minikube/hosts_windows.go
+++ pkg/kyma/cmd/provision/minikube/hosts_windows.go
@@ -4,6 +4,7 @@ package minikube
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/kyma-project/cli/internal/step"
 )
@@ -11,7 +12,26 @@ import (
 func addDevDomainsToEtcHostsOSSpecific(o *MinikubeOptions, s step.Step, hostAlias string) error {
 
 	s.LogErrorf("Please add these lines to your " + hostsFile + " file:")
-	fmt.Println(hostAlias)
+	hostsArray := strings.Split(hostAlias, " ")
+	ip := hostsArray[0]
+	hostsPerLine := split(hostsArray[1:], 7) // 7 hostnames per line
+
+	for k := 0; k < len(hostsPerLine); k++ {
+		fmt.Printf("%s %s\n", ip, strings.Join(hostsPerLine[k], " "))
+	}
 
 	return nil
+}
+
+func split(buf []string, lim int) [][]string {
+	var chunk []string
+	chunks := make([][]string, 0, len(buf)/lim+1)
+	for len(buf) >= lim {
+		chunk, buf = buf[:lim], buf[lim:]
+		chunks = append(chunks, chunk)
+	}
+	if len(buf) > 0 {
+		chunks = append(chunks, buf[:len(buf)])
+	}
+	return chunks
 }
