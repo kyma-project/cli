0bee771156b793fa2fb0878c8eb96f09b6a51fc6
Borja Clemente <borja.clemente.castanera@sap.com>
2019-07-05 17:18:07 +0200

    Fix passing kubeconfig flag to kubectl

--- internal/kubectl/wrapper.go
+++ internal/kubectl/wrapper.go
@@ -1,17 +1,24 @@
 package kubectl
 
+import "fmt"
+
 type Wrapper struct {
-	verbose bool
+	verbose    bool
+	kubeconfig string
 }
 
-func NewWrapper(verbose bool) *Wrapper {
-	return &Wrapper{verbose}
+func NewWrapper(verbose bool, kubeconfig string) *Wrapper {
+	return &Wrapper{
+		verbose:    verbose,
+		kubeconfig: kubeconfig,
+	}
 }
 
 func (w *Wrapper) RunCmd(args ...string) (string, error) {
+	args = append(args, fmt.Sprintf("--kubeconfig=%s", w.kubeconfig))
 	return runCmd(w.verbose, args...)
 }
 
 func (w *Wrapper) RunApplyCmd(resources []map[string]interface{}) (string, error) {
-	return runApplyCmd(resources, w.verbose)
+	return runApplyCmd(resources, w.verbose, w.kubeconfig)
 }
