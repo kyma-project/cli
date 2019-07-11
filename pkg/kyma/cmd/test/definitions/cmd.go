5b3172c787bbd7206ff5665e9b02c75630010ac1
Igor <LinkKellerman@gmail.com>
2019-07-11 13:41:50 +0200

    Update pkg/kyma/cmd/test/definitions/cmd.go
    Co-Authored-By: Barbara Szwarc <barbara.m.szwarc@gmail.com>

--- pkg/kyma/cmd/test/definitions/cmd.go
+++ pkg/kyma/cmd/test/definitions/cmd.go
@@ -23,7 +23,7 @@ func NewCmd(o *options) *cobra.Command {
 
 	cobraCmd := &cobra.Command{
 		Use:     "defintions",
-		Short:   "Show available test defintions on a running Kyma cluster",
+		Short:   "Show test definitions available for a provisioned Kyma cluster",
 		Long:    `Show available test defintions on a running Kyma cluster`,
 		RunE:    func(_ *cobra.Command, _ []string) error { return cmd.Run() },
 		Aliases: []string{"def"},
