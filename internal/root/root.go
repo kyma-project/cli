a6378aff4cbf541221567f5ccbe4b9321a521f30
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-10-09 20:21:24 +0200

    Use upper case
    Use upper case for default values when asking user for confirmation 
    https://github.com/kyma-project/cli/issues/255

--- internal/root/root.go
+++ internal/root/root.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func PromptUser() bool {
 	for {
-		fmt.Print("Type [y/n]: ")
+		fmt.Print("Type [Y/n]: ")
 		var res string
 		if _, err := fmt.Scanf("%s", &res); err != nil {
 			return false
 		}
 		switch res {
-		case "yes", "y":
+		case "Yes", "Y":
 			return true
 		case "no", "n":
 			return false
