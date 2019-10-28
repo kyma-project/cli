bb8ebd4a9b6303f11fd788a5a2e978b8b9d7eb45
Suleyman Akbas <suleyman.akbas@sap.com>
2019-10-28 11:29:39 +0100

    Add shell completion for zsh (#181)

--- cmd/kyma/completion/completion.go
+++ cmd/kyma/completion/completion.go
@@ -1,20 +1,22 @@
 package completion
 
 import (
+	"fmt"
+	"io"
 	"os"
+
+	"github.com/pkg/errors"
 
 	"github.com/spf13/cobra"
 )
 
-//NewCompletionCmd creates a new completion command
+//NewCmd creates a new completion command
 func NewCmd() *cobra.Command {
 	var completionCmd = &cobra.Command{
-		Use:   "completion",
-		Short: "Generates bash completion scripts.",
+		Use:   "completion bash|zsh",
+		Short: "Generates bash or zsh completion scripts.",
 		Long: `Use this command to display the shell completion code used for interactive command completion. 
-To configure your bash shell to load completions, add ` + "`. <(kyma completion)`" + ` to your bash profile.
-
- 
+To configure your shell to load completions, add ` + "`. <(kyma completion bash)`" + ` to your bash profile or ` + "`. <(kyma completion zsh)`" + ` to your zsh profile.
 `,
 		RunE:    completion,
 		Aliases: []string{},
@@ -24,6 +26,34 @@ To configure your bash shell to load completions, add ` + "`. <(kyma completion)
 }
 
 func completion(cmd *cobra.Command, args []string) error {
-	err := cmd.GenBashCompletion(os.Stdout)
+	if len(args) != 1 {
+		fmt.Println("Usage: kyma completion bash|zsh")
+		fmt.Println("See 'kyma completion -h' for help")
+		return nil
+	}
+
+	switch shell := args[0]; shell {
+	case "bash":
+		err := cmd.GenBashCompletion(os.Stdout)
+		return errors.Wrap(err, "Error generating bash completion")
+	case "zsh":
+		err := genZshCompletion(cmd, os.Stdout)
+		return errors.Wrap(err, "Error generating zsh completion")
+	default:
+		fmt.Printf("Sorry, completion is not supported for %q", shell)
+	}
+
+	return nil
+}
+
+func genZshCompletion(cmd *cobra.Command, out io.Writer) error {
+	err := cmd.GenZshCompletion(out)
+	if err != nil {
+		return err
+	}
+
+	zshCompdef := "\ncompdef _kyma kyma\n"
+
+	_, err = io.WriteString(out, zshCompdef)
 	return err
 }
