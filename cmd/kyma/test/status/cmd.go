2efa00a979a387ef1c4a36a55651abaa2e41cbd3
I332735 <suleyman.akbas@sap.com>
2019-11-27 11:30:00 +0100

    Fix typo on test run command docs

--- cmd/kyma/test/status/cmd.go
+++ cmd/kyma/test/status/cmd.go
@@ -35,8 +35,8 @@ func NewCmd(o *options) *cobra.Command {
 		Long: `Use this command to display the status of a test suite and related test executions.
 
 If you don't provide any arguments, the status of all test suites will be printed.
-To print the status of all test suites, ` + "`run kyma test status`" + `.
-To print the status of specific test cases, ` + "`run kyma test status testSuiteOne testSuiteTwo`" + `.
+To print the status of all test suites, run ` + "`kyma test status`" + `.
+To print the status of specific test cases, run ` + "`kyma test status testSuiteOne testSuiteTwo`" + `.
 `,
 
 		RunE:    func(_ *cobra.Command, args []string) error { return cmd.Run(args) },
