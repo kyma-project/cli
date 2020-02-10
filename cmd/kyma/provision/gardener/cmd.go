7e319629966cfe285f4a005a065569cfa99af599
Rakesh Garimella <rakesh.garimella@sap.com>
2020-02-10 13:55:09 +0100

    Add support for service endpoints in Azure

--- cmd/kyma/provision/gardener/cmd.go
+++ cmd/kyma/provision/gardener/cmd.go
@@ -65,7 +65,7 @@ Use the following instructions to create a service account for a selected provid
 	cmd.Flags().StringVar(&o.NetworkServices, "network-services", "", "CIDR of the service network.")
 	cmd.Flags().StringVar(&o.MachineImageName, "machine-image-name", "coreos", "Version of the shoot's machine image name in any environment.")
 	cmd.Flags().StringVar(&o.MachineImageVersion, "machine-image-version", "2303.3.0", "Version of the shoot's machine image version in any environment.")
-	cmd.Flags().StringSliceVar(&o.ServiceEndpoints, "service-endpoints", nil, "list of Azure ServiceEndpoints which should be associated with the worker subnet.")
+	cmd.Flags().StringSliceVar(&o.ServiceEndpoints, "service-endpoints", nil, "list of Azure ServiceEndpoints which should be associated with the worker subnet. eg. --service-endpoints=\"az1,az2\"")
 	cmd.Flags().StringSliceVarP(&o.Extra, "extra", "e", nil, "One or more arguments provided as the `NAME=VALUE` key-value pairs to configure additional cluster settings. You can use this flag multiple times or enter the key-value pairs as a comma-separated list.")
 
 	return cmd
@@ -142,8 +142,6 @@ func newProvider(o *Options) (*types.Provider, error) {
 		CredentialsFilePath: o.CredentialsFile,
 	}
 
-	workerName := o.Name + "-worker"
-
 	p.CustomConfigurations = make(map[string]interface{})
 	if o.Secret != "" {
 		p.CustomConfigurations["target_secret"] = o.Secret
@@ -156,7 +154,6 @@ func newProvider(o *Options) (*types.Provider, error) {
 	p.CustomConfigurations["worker_max_unavailable"] = o.Unavailable
 	p.CustomConfigurations["vnetcidr"] = o.CIDR
 	p.CustomConfigurations["workercidr"] = o.WCIDR
-	p.CustomConfigurations["worker_name"] = workerName
 	p.CustomConfigurations["networking_nodes"] = o.NetworkNodes
 	p.CustomConfigurations["networking_pods"] = o.NetworkPods
 	p.CustomConfigurations["networking_services"] = o.NetworkServices
--- cmd/kyma/provision/gardener/opts.go
+++ cmd/kyma/provision/gardener/opts.go
@@ -5,32 +5,32 @@ import "github.com/kyma-project/cli/internal/cli"
 type Options struct {
 	*cli.Options
 
-	Name              string
-	Project           string
-	CredentialsFile   string
-	TargetProvider    string
-	Secret            string
-	KubernetesVersion string
-	Region            string
-	Zone              string
-	MachineType       string
-	CIDR              string
-	WCIDR             string
-	DiskType          string
-	DiskSizeGB        int
-	NodeCount         int
-	ScalerMin         int
-	ScalerMax         int
-	Surge             int
-	Unavailable       int
-	Extra             []string
-	NetworkType       string
-	NetworkNodes      string
-	NetworkPods       string
-	NetworkServices   string
-	MachineImageName string
+	Name                string
+	Project             string
+	CredentialsFile     string
+	TargetProvider      string
+	Secret              string
+	KubernetesVersion   string
+	Region              string
+	Zone                string
+	MachineType         string
+	CIDR                string
+	WCIDR               string
+	DiskType            string
+	DiskSizeGB          int
+	NodeCount           int
+	ScalerMin           int
+	ScalerMax           int
+	Surge               int
+	Unavailable         int
+	Extra               []string
+	NetworkType         string
+	NetworkNodes        string
+	NetworkPods         string
+	NetworkServices     string
+	MachineImageName    string
 	MachineImageVersion string
-	ServiceEndpoints []string
+	ServiceEndpoints    []string
 }
 
 //NewOptions creates options with default values
