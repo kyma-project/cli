b06dd131240b71891b8dadca6a8284dd9306546b
Rakesh Garimella <rakesh.garimella@sap.com>
2020-02-12 14:22:48 +0100

    Fix tests and regenerate docs

--- cmd/kyma/provision/gardener/cmd_test.go
+++ cmd/kyma/provision/gardener/cmd_test.go
@@ -20,7 +20,7 @@ func TestProvisionGardenerFlags(t *testing.T) {
 	require.Equal(t, "gcp", o.TargetProvider, "The parsed value for the credentials flag should gcp")
 	require.Equal(t, "", o.Secret, "The parsed value for the secret flag not as expected.")
 	require.Equal(t, "1.15.4", o.KubernetesVersion, "Default value for the kube-version flag not as expected.")
-	require.Equal(t, "westeurope", o.Region, "Default value for the region flag not as expected.")
+	require.Equal(t, "europe-west3", o.Region, "Default value for the region flag not as expected.")
 	require.Equal(t, "europe-west3-a", o.Zone, "Default value for the zone flag not as expected.")
 	require.Equal(t, "n1-standard-4", o.MachineType, "Default value for the type flag not as expected.")
 	require.Equal(t, 30, o.DiskSizeGB, "Default value for the disk-size flag not as expected.")
