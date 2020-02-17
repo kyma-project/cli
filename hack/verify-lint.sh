eedeb6d8fa313beec3880c11f922fa599b49afe4
I332735 <suleyman.akbas@sap.com>
2020-02-17 13:37:38 +0100

    Improve

--- hack/verify-lint.sh
+++ hack/verify-lint.sh
@@ -48,7 +48,8 @@ golangci::run_checks() {
 main() {
   result=$(git status --porcelain)
   if [[ "${result}" != "" ]]; then
-    echo "git is currently in a dirty state: ${result}"
+    echo "ERROR: git is currently in a dirty state:" 
+    echo "${result}"
     exit 1
   fi
 
