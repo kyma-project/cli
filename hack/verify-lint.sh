c05a59466a31336a8ec52200b025693fc695c68c
Suleyman Akbas <suleyman.akbas@sap.com>
2020-02-17 14:22:55 +0100

    Fail when git state is dirty (#374)

--- hack/verify-lint.sh
+++ hack/verify-lint.sh
@@ -46,6 +46,13 @@ golangci::run_checks() {
 }
 
 main() {
+  result=$(git status --porcelain)
+  if [[ "${result}" != "" ]]; then
+    echo "ERROR: git is currently in a dirty state:" 
+    echo "${result}"
+    exit 1
+  fi
+
   if [[ "${SKIP_INSTALL:-x}" != "true" ]]; then
     export INSTALL_DIR=${TMP_DIR}
     golangci::install
