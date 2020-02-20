b656307dc358d4b345318cec135fa7ee031f9ff2
Rakesh Garimella <rakesh.garimella@sap.com>
2020-02-20 15:02:27 +0100

    Add graceful shutdown to kyma cli
     handle ctrl-c

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/kyma-project/cli/cmd/kyma"
 	"github.com/kyma-project/cli/internal/cli"
 )
 
 func main() {
+	SetupCloseHandler()
 	command := kyma.NewCmd(cli.NewOptions())
 
 	err := command.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
+
+}
+
+func SetupCloseHandler() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGINT)
+	go func() {
+		<-c
+		fmt.Println("\r- Ctrl+C pressed in Terminal. Exiting...")
+		os.Exit(0)
+	}()
 }
