3f2edb4b7c7b7518a5b72165e9b4a9aca89e6d51
Mostafa Shorim <mostafa.shorim@sap.com>
2020-03-04 14:55:33 +0100

    debugging

--- hack/verify-generated-docs.sh
+++ hack/verify-generated-docs.sh
@@ -4,6 +4,7 @@
 set -o nounset # treat unset variables as an error and exit immediately.
 set -o errexit # exit immediately when a command fails.
 set -E         # needs to be set if we want the ERR trap
+set -e
 
 readonly CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
