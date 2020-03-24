da7276e1460e04db053b9e97b3ec6e7e006fe000
Mostafa Shorim <mostafa.shorim@sap.com>
2020-03-24 12:52:31 +0100

    adding a clean up to remove docs changes in git

--- hack/verify-generated-docs.sh
+++ hack/verify-generated-docs.sh
@@ -3,10 +3,17 @@
 # standard bash error handling
 set -o nounset # treat unset variables as an error and exit immediately.
 set -o errexit # exit immediately when a command fails.
+set -E         # needs to be set if we want the ERR trap
 
 readonly CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source "${CURRENT_DIR}/utilities.sh" || { echo 'Cannot load CI utilities.'; exit 1; }
+
+clean_up() {
+    git reset --hard
+}
+
+trap cleanup EXIT SIGINT
 
 generate_docs() {
     shout "Auto generate docs"
@@ -43,6 +50,7 @@ check_generated_docs() {
 main() {
     generate_docs
     check_generated_docs
+    clean_up
 }
 
 main
