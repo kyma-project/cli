bcbecaba0e0a44d1490554b57c4991a6c26e63f3
I332735 <suleyman.akbas@sap.com>
2020-06-15 11:39:47 +0200

    Fix linter

--- internal/files/files.go
+++ internal/files/files.go
@@ -2,13 +2,9 @@
 package files
 
 import (
-	"io/ioutil"
 	"os"
 	"os/user"
 	"path/filepath"
-	fp "path/filepath"
-
-	"github.com/pkg/errors"
 )
 
 const kymaFolder = ".kyma"
@@ -27,35 +23,4 @@ func KymaHome() (string, error) {
 		}
 	}
 	return p, nil
-}
-
-// Save saves the given content to the relative file path inside the kyma CLI local folder
-func Save(filePath string, content []byte) error {
-	kh, err := KymaHome()
-	if err != nil {
-		return errors.Wrap(err, "Could not save file")
-	}
-
-	filePath = fp.Join(kh, filePath)
-
-	err = os.MkdirAll(fp.Dir(filePath), 0700)
-	if err != nil {
-		return errors.Wrap(err, "Could not save file")
-	}
-
-	if err = ioutil.WriteFile(filePath, content, 0644); err != nil {
-		return errors.Wrap(err, "Could not save file")
-	}
-	return nil
-}
-
-// Load loads the content of the relative file path inside the kyma CLI local folder
-func Load(filePath string) ([]byte, error) {
-	kh, err := KymaHome()
-	if err != nil {
-		return nil, errors.Wrap(err, "Could not load file")
-	}
-
-	filePath = fp.Join(kh, filePath)
-	return ioutil.ReadFile(filePath)
 }
