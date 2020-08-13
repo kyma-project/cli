081e8b749955f32660b28ee73ded12cb5b5ff095
Mostafa Shorim <mostafa.shorim@sap.com>
2020-08-13 13:05:20 +0200

    disabling upgrade command (#553)
    * disabling upgrade command
    
    * update docs to exclude upgrade command
    
    * changing number of kyma subcommands in kyma_test
    
    * removing commented code for adding the upgrade command

--- cmd/kyma/kyma.go
+++ cmd/kyma/kyma.go
@@ -21,7 +21,6 @@ import (
 	"github.com/kyma-project/cli/cmd/kyma/version"
 
 	"github.com/kyma-project/cli/cmd/kyma/provision"
-	"github.com/kyma-project/cli/cmd/kyma/upgrade"
 	"github.com/kyma-project/cli/internal/cli"
 	"github.com/spf13/cobra"
 )
@@ -64,7 +63,6 @@ For more information, see: https://github.com/kyma-project/cli
 		install.NewCmd(install.NewOptions(o)),
 		provisionCmd,
 		console.NewCmd(console.NewOptions(o)),
-		upgrade.NewCmd(upgrade.NewOptions(o)),
 	)
 
 	testCmd := test.NewCmd()
