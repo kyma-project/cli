f1b97531783fd50f9f7e168f557790e790df6166
Szwarc, Barbara <barbara.szwarc@sap.com>
2020-08-14 13:32:50 +0200

    Improve CLI docs

--- cmd/kyma/kyma.go
+++ cmd/kyma/kyma.go
@@ -34,7 +34,6 @@ func NewCmd(o *cli.Options) *cobra.Command {
 		Long: `Kyma is a flexible and easy way to connect and extend enterprise applications in a cloud-native world.
 Kyma CLI allows you to install, test, and manage Kyma.
 
-For more information, see: https://github.com/kyma-project/cli
 `,
 		// Affects children as well
 		SilenceErrors: false,
