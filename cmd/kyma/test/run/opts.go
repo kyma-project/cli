33e45cb9ff60fc2b80f502873618628ea2feb425
Korbinian Stoemmer <github@k15r.dev>
2020-08-21 12:47:08 +0200

    Support matchlabelexpression for test definitions (#560)

--- cmd/kyma/test/run/opts.go
+++ cmd/kyma/test/run/opts.go
@@ -8,12 +8,13 @@ import (
 
 type Options struct {
 	*cli.Options
-	Name           string
-	Watch          bool
-	Timeout        time.Duration
-	ExecutionCount int64
-	MaxRetries     int64
-	Concurrency    int64
+	Name             string
+	Watch            bool
+	Timeout          time.Duration
+	ExecutionCount   int64
+	MaxRetries       int64
+	Concurrency      int64
+	LabelExpressions []string
 }
 
 func NewOptions(o *cli.Options) *Options {
