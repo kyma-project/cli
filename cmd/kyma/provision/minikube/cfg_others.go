4e95e2ffb07ec0c8e2456a649e5caa6a709cdc08
Rakesh Garimella <rakesh.garimella@sap.com>
2020-09-30 14:09:36 +0200

    Add the info about vpn sock flag

--- cmd/kyma/provision/minikube/cfg_others.go
+++ cmd/kyma/provision/minikube/cfg_others.go
@@ -3,13 +3,21 @@
 
 package minikube
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 const defaultVMDriver = vmDriverNone
 
 func osSpecificFlags(o *Options, cmd *cobra.Command) {
+	cmd.Flags().BoolVar(&o.UseVPNKitSock, "use-hyperkit-vpnkit-sock", false, `Uses vpnkit sock provided by Docker. This is useful when DNS Port (53) is being used by some other program like dns-proxy (eg. provided by Cisco Umbrella. It works only on mac OS).`)
 }
 
 func osSpecificRun(c *command, startCmd []string) ([]string, error) {
+	if c.opts.UseVPNKitSock {
+		fmt.Println("This flag is only supported for Mac OS!")
+	}
 	return startCmd, nil
 }
