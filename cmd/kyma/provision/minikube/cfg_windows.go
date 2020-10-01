2b22b2329120db98b06f7ae2b93ae1b5d6ee29e1
Rakesh Garimella <Rakesh.garimella@sap.com>
2020-10-01 11:25:18 +0200

    Forward the dns requests to dns-proxy if present (#555)
    * Forward the dns requests to dns-proxy if present
    
    - There can be instances when there is a dns-proxy eg Cisco Umbrella running on the
      dns port (Port 53). This is usually coming from corporate requirements. In such a
      case we should use this proxy to resolve the dns-requests.
    - If we dont use the dns-proxy then it can lead to failure of dns resolution by minikube
      and thus the images wont be pulled from the registry.
    
    * Add test file to minikube provision
    
    * Update docs
    
    * Apply suggestions from code review
    
    Co-authored-by: Barbara Szwarc <barbara.m.szwarc@gmail.com>
    
    * Display vpnSock flag only for mac
    
    * Changes based on review commnents
    
    * Remove extra space
    
    Co-authored-by: Barbara Szwarc <barbara.m.szwarc@gmail.com>

--- cmd/kyma/provision/minikube/cfg_windows.go
+++ cmd/kyma/provision/minikube/cfg_windows.go
@@ -2,4 +2,14 @@
 
 package minikube
 
+import "github.com/kyma-project/cli/internal/nice"
+
 const defaultVMDriver = vmDriverVirtualBox
+
+func osSpecificRun(c *command, startCmd []string) ([]string, error) {
+	if c.opts.UseVPNKitSock {
+		np := nice.Nice{}
+		np.PrintImportant("WARNING: This flag is supported only for Mac OS!")
+	}
+	return startCmd, nil
+}
---
+++ cmd/kyma/provision/minikube/cmd_test.go
@@ -0,0 +1,71 @@
+package minikube
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kyma-project/cli/pkg/step"
+
+	"github.com/kyma-project/cli/internal/cli"
+	"github.com/stretchr/testify/require"
+)
+
+// TestProvisionGKEFlags ensures that the provided command flags are stored in the options.
+func TestProvisionMinikubeFlags(t *testing.T) {
+	o := NewOptions(&cli.Options{})
+	c := NewCmd(o)
+
+	// test passing flags
+	err := c.ParseFlags([]string{
+		"--cpus", "6",
+		"--memory", "4096",
+		"--vm-driver", "kvm",
+		"--profile", "fooProfile",
+	})
+	require.NoError(t, err, "Parsing flags should not return an error")
+	require.Equal(t, "6", o.CPUS)
+	require.Equal(t, "4096", o.Memory)
+	require.Equal(t, "kvm", o.VMDriver)
+	require.Equal(t, "fooProfile", o.Profile)
+}
+
+func TestCheckRequirements(t *testing.T) {
+
+	cases := []struct {
+		name        string
+		shouldFail  bool
+		expectedErr string
+		op          Options
+	}{
+		{
+			name:        "VM Driver not supported",
+			shouldFail:  true,
+			expectedErr: "Specified VMDriver 'fooDriver' is not supported by Minikube",
+			op: Options{
+				VMDriver: "fooDriver",
+			},
+		},
+		{
+			name:        "VM Driver requires hypervVirtualSwitch",
+			shouldFail:  true,
+			expectedErr: "Specified VMDriver 'hyperv' requires the --hypervVirtualSwitch option",
+			op: Options{
+				VMDriver: "hyperv",
+			},
+		},
+	}
+	var step step.Factory
+	s := step.NewStep("checking requirements")
+	for _, c := range cases {
+		opts := &c.op
+		cmd := command{
+			opts: opts,
+		}
+
+		err := cmd.checkRequirements(s)
+		if c.expectedErr != "" {
+			require.EqualError(t, err, c.expectedErr, fmt.Sprintf("Test Case: %s", c.name))
+		}
+
+	}
+}
