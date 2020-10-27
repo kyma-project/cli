17ad7c21f2d957905ea08c3c8b404ad8d3b80ad4
Karol Furgoł <furgolkarol@gmail.com>
2020-10-27 14:38:23 +0100

    review fixes

--- cmd/kyma/sync/function/opts.go
+++ cmd/kyma/sync/function/opts.go
@@ -2,9 +2,10 @@ package function
 
 import (
 	"fmt"
-	"github.com/kyma-project/cli/internal/cli"
 	"os"
 	"time"
+
+	"github.com/kyma-project/cli/internal/cli"
 )
 
 //Options defines available options for the command
@@ -31,22 +32,13 @@ func (o *Options) setDefaults(defaultNamespace string) (err error) {
 		}
 	}
 
-	setIfZero(&o.Namespace, defaultNamespace, "default")
+	if o.Namespace == "" {
+		o.Namespace = defaultNamespace
+	}
 
 	if o.Name == "" {
 		return fmt.Errorf("flag 'name' is required")
 	}
 
 	return
-}
-
-func setIfZero(val *string, defaultValue string, defaultValues ...string) bool {
-	for _, v := range append([]string{defaultValue}, defaultValues...) {
-		if v != "" {
-			continue
-		}
-		*val = v
-		return true
-	}
-	return false
 }
