3024e47b24695cc925c415034b86e2f741b77a32
I332735 <suleyman.akbas@sap.com>
2020-11-27 15:53:44 +0100

    Fix test

--- cmd/kyma/kyma_test.go
+++ cmd/kyma/kyma_test.go
@@ -38,5 +38,5 @@ func TestKymaSubcommands(t *testing.T) {
 
 	sub := c.Commands()
 
-	require.Equal(t, 12, len(sub), "Number of Kyma subcommands not as expected")
+	require.Equal(t, 13, len(sub), "Number of Kyma subcommands not as expected")
 }
