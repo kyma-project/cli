dc53524959bbe8a4bd3c095ceb078bb1b50ee857
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2020-12-16 18:12:24 +0100

    Remove errors.Wrapf fct with fmt.Errors

--- internal/k3s/k3s.go
+++ internal/k3s/k3s.go
@@ -29,7 +29,7 @@ func RunCmd(verbose bool, timeout time.Duration, args ...string) (string, error)
 		if verbose {
 			fmt.Printf("Failing command:\n  k3d %s\nwith output:\n  %s\nand error:\n  %s\n", strings.Join(args, " "), string(out), err)
 		}
-		return out, errors.Wrapf(err, "Executing 'k3d %s' failed with output '%s'", strings.Join(args, " "), out)
+		return out, fmt.Errorf("Executing 'k3d %s' failed with output '%v'", strings.Join(args, " "), out)
 	}
 
 	if ctx.Err() == context.DeadlineExceeded {
