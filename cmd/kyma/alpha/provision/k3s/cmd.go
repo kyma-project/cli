2d19fcfa3dc0cb1f2513e0749c44c1f7063cb8ec
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-02-10 18:16:24 +0100

    Rename flag server-arg to server-args

--- cmd/kyma/alpha/provision/k3s/cmd.go
+++ cmd/kyma/alpha/provision/k3s/cmd.go
@@ -37,7 +37,7 @@ func NewCmd(o *Options) *cobra.Command {
 	//cmd.Flags().StringVar(&o.EnableRegistry, "enable-registry", "", "Enables registry for the created k8s cluster.")
 	cmd.Flags().StringVar(&o.Name, "name", "kyma", "Name of the Kyma cluster.")
 	cmd.Flags().IntVar(&o.Workers, "workers", 1, "Number of worker nodes.")
-	cmd.Flags().StringSliceVarP(&o.ServerArgs, "server-arg", "s", []string{}, "Argument passed to the Kubernetes server (e.g. --server-arg='--alsologtostderr').")
+	cmd.Flags().StringSliceVarP(&o.ServerArgs, "server-args", "s", []string{}, "Arguments passed to the Kubernetes server (e.g. --server-args='--alsologtostderr').")
 	cmd.Flags().DurationVar(&o.Timeout, "timeout", 5*time.Minute, `Maximum time in minutes during which the provisioning takes place, where "0" means "infinite".`)
 	return cmd
 }
