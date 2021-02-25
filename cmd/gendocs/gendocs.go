d7e28d5eacf95394f7470090ba35280e24df58e1
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-02-25 16:48:23 +0100

    Rename options to flags in docu

--- cmd/gendocs/gendocs.go
+++ cmd/gendocs/gendocs.go
@@ -111,7 +111,7 @@ func printOptions(buf *bytes.Buffer, cmd *cobra.Command) error {
 	flags := cmd.NonInheritedFlags()
 	flags.SetOutput(buf)
 	if flags.HasAvailableFlags() {
-		buf.WriteString("## Options\n\n```bash\n")
+		buf.WriteString("## Flags\n\n```bash\n")
 		flags.PrintDefaults()
 		buf.WriteString("```\n\n")
 	}
@@ -119,7 +119,7 @@ func printOptions(buf *bytes.Buffer, cmd *cobra.Command) error {
 	parentFlags := cmd.InheritedFlags()
 	parentFlags.SetOutput(buf)
 	if parentFlags.HasAvailableFlags() {
-		buf.WriteString("## Options inherited from parent commands\n\n```bash\n")
+		buf.WriteString("## Flags inherited from parent commands\n\n```bash\n")
 		parentFlags.PrintDefaults()
 		buf.WriteString("```\n\n")
 	}
