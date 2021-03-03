a704a7358d094f1c3628d88706f72a137f1ddd83
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-03-03 14:29:49 +0100

    Avoid duplicate code

--- cmd/kyma/alpha/deploy/cmd.go
+++ cmd/kyma/alpha/deploy/cmd.go
@@ -195,8 +195,9 @@ func (cmd *command) isCompatibleVersion() error {
 }
 
 func (cmd *command) deployKyma(ui asyncui.AsyncUI, overrides *deployment.OverridesBuilder) error {
-	var resourcePath = filepath.Join(cmd.opts.ResolveLocalWorkspacePath(), "resources")
-	var installResourcePath = filepath.Join(cmd.opts.ResolveLocalWorkspacePath(), "installation", "resources")
+	localWorkspace := cmd.opts.ResolveLocalWorkspacePath()
+	resourcePath := filepath.Join(localWorkspace, "resources")
+	installResourcePath := filepath.Join(localWorkspace, "installation", "resources")
 
 	cmpFile, err := cmd.opts.ResolveComponentsFile()
 	if err != nil {
