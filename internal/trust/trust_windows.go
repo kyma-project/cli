1246865c0c8857c5e6b0fc74753b95edda36ef29
Borja Clemente <borja.clemente.castanera@sap.com>
2021-03-11 16:52:34 +0100

    Fix certificate import for new installation
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>

--- internal/trust/trust_windows.go
+++ internal/trust/trust_windows.go
@@ -4,7 +4,6 @@ package trust
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
 
 	"github.com/kyma-project/cli/internal/cli"
@@ -25,17 +24,12 @@ func NewCertifier(k kube.KymaKube) Certifier {
 }
 
 func (c certutil) Certificate() ([]byte, error) {
-	cm, err := c.k8s.Static().CoreV1().ConfigMaps("kyma-installer").Get(context.Background(), "net-global-overrides", metav1.GetOptions{})
+	s, err := c.k8s.Static().CoreV1().Secrets("istio-system").Get(context.Background(), "kyma-gateway-certs", metav1.GetOptions{})
 	if err != nil {
 		return nil, errors.Wrap(err, fmt.Sprintf("\nCould not retrieve the Kyma root certificate. Follow the instructions to import it manually:\n-----\n%s-----\n", c.Instructions()))
 	}
 
-	decodedCert, err := base64.StdEncoding.DecodeString(cm.Data["global.ingress.tlsCrt"])
-	if err != nil {
-		return nil, err
-	}
-
-	return decodedCert, nil
+	return s.Data["tls.crt"], nil
 }
 
 func (c certutil) StoreCertificate(file string, i Informer) error {
@@ -56,7 +50,7 @@ func (c certutil) StoreCertificate(file string, i Informer) error {
 
 func (certutil) Instructions() string {
 	return "1. Open a terminal window with administrator rights.\n" +
-		"2. Download the certificate: kubectl get secret kyma-gateway-certs -n istio-system -o jsonpath='{.data.cert}' > tmp.txt\n" +
+		"2. Download the certificate: kubectl get secret kyma-gateway-certs -n istio-system -o jsonpath='{.data.tls\\.crt}' > tmp.txt\n" +
 		"3. Decode the certificate: certutil -decode tmp.txt kyma.crt ; del tmp.txt\n" +
 		"4. Import the certificate: certutil -addstore -f Root kyma.crt\n"
 }
