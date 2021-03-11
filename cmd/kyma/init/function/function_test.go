fd6057c37dade5427406ec52e89b62c272a0ccc0
Rafał Jankowski <rafal.jankowski.93@gmail.com>
2021-03-11 11:11:57 +0100

    Add auto generated name for function (#775)

--- cmd/kyma/init/function/function_test.go
+++ cmd/kyma/init/function/function_test.go
@@ -14,12 +14,12 @@ func TestFunctionFlags(t *testing.T) {
 	c := NewCmd(o)
 
 	// test default flag values
-	require.Equal(t, "first-function", o.Name, "Default value for the --name flag not as expected.")
+	require.Equal(t, "", o.Name, "Default value for the --name flag not as expected.")
 	require.Equal(t, "", o.Namespace, "Default value for the --namespace flag not as expected.")
 	require.Equal(t, "", o.Dir, "Default value for the --dir flag not as expected.")
 	require.Equal(t, "nodejs12", o.Runtime, "Default value for the --runtime flag not as expected.")
 	require.Equal(t, "", o.URL, "The parsed value for the --url flag not as expected.")
-	require.Equal(t, o.Name, o.RepositoryName.String(), "The parsed value for the --repository-name flag not as expected.")
+	require.Equal(t, "", o.RepositoryName, "The parsed value for the --repository-name flag not as expected.")
 	require.Equal(t, "master", o.Reference, "The parsed value for the --reference flag not as expected.")
 	require.Equal(t, "/", o.BaseDir, "The parsed value for the --base-dir flag not as expected.")
 
@@ -40,7 +40,7 @@ func TestFunctionFlags(t *testing.T) {
 	require.Equal(t, "test-namespace", o.Namespace, "The parsed value for the --namespace flag not as expected.")
 	require.Equal(t, "python38", o.Runtime, "The parsed value for the --runtime flag not as expected.")
 	require.Equal(t, "test-url", o.URL, "The parsed value for the --url flag not as expected.")
-	require.Equal(t, "test-repository-name", o.RepositoryName.String(), "The parsed value for the --repository-name flag not as expected.")
+	require.Equal(t, "test-repository-name", o.RepositoryName, "The parsed value for the --repository-name flag not as expected.")
 	require.Equal(t, "test-reference", o.Reference, "The parsed value for the --reference flag not as expected.")
 	require.Equal(t, "test-base-dir", o.BaseDir, "The parsed value for the --base-dir flag not as expected.")
 
@@ -54,7 +54,7 @@ func TestFunctionFlags(t *testing.T) {
 	require.Equal(t, "test-namespace", o.Namespace, "The parsed value for the --namespace flag not as expected.")
 	require.Equal(t, "nodejs10", o.Runtime, "The parsed value for the --runtime flag not as expected.")
 	require.Equal(t, "test-url", o.URL, "The parsed value for the --url flag not as expected.")
-	require.Equal(t, "test-repository-name", o.RepositoryName.String(), "The parsed value for the --repository-name flag not as expected.")
+	require.Equal(t, "test-repository-name", o.RepositoryName, "The parsed value for the --repository-name flag not as expected.")
 	require.Equal(t, "test-reference", o.Reference, "The parsed value for the --reference flag not as expected.")
 	require.Equal(t, "test-base-dir", o.BaseDir, "The parsed value for the --base-dir flag not as expected.")
 }
