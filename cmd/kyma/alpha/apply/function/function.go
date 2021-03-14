bb2ca3853a01cdd1ab4afae5e86a36f08adbfdea
pPrecel <filip.strozik@outlook.com>
2021-03-14 22:20:18 +0100

    Update deps

--- cmd/kyma/alpha/apply/function/function.go
+++ cmd/kyma/alpha/apply/function/function.go
@@ -100,10 +100,16 @@ func (c *command) Run() error {
 	mgr.AddParent(
 		operator.NewGenericOperator(client.Resource(operator.GVKFunction).Namespace(configuration.Namespace), function),
 		[]operator.Operator{
-			operator.NewSubscriptionOperator(client.Resource(operator.GVKSubscription).Namespace(configuration.Namespace),
+			operator.NewSubscriptionsOperator(client.Resource(operator.GVKSubscription).Namespace(configuration.Namespace),
 				configuration.Name, configuration.Namespace, subscriptions...),
 		},
 	)
+
+	oprs := map[string][]string{}
+
+	for key, _ := range oprs {
+		fmt.Print(key)
+	}
 
 	options := manager.Options{
 		Callbacks:          callbacks(c),
