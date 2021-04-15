060e4c9ec3b00f2ec7bf3526e4661cb43485593b
I332735 <suleyman.akbas@sap.com>
2021-04-15 10:01:34 +0200

    Fix instructions

--- internal/trust/trust_darwin.go
+++ internal/trust/trust_darwin.go
@@ -68,7 +68,7 @@ func (k keychain) StoreCertificate(file string, i Informer) error {
 }
 
 func (keychain) Instructions() string {
-	return "1. Download the certificate: kubectl get secret kyma-gateway-certs -n istio-system -o jsonpath='{.data.cert}' > kyma.crt\n" +
+	return "1. Download the certificate: kubectl get configmap net-global-overrides -n kyma-installer -o jsonpath='{.data.global\\.ingress\\.tlsCrt}' | base64 --decode > kyma.crt\n" +
 		"2. Import the certificate: sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain kyma.crt\n"
 }
 
