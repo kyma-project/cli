ec50091e8451449519339c5f900daf9117195690
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-04-22 09:41:52 +0200

    Update pkg/installation/patchversion_test.go
    Co-authored-by: Jeremy Harisch <48282931+JeremyHarisch@users.noreply.github.com>

--- pkg/installation/patchversion_test.go
+++ pkg/installation/patchversion_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestFindLatestPatch(t *testing.T) {
 	version := "1.7.0"
-	versions := []string{"1.6.21", "1.7.0/config.yaml", "1.7.10", "1.7.11-rc1/kyma-installer-cr-cluster.yaml", "1.7.9"}
-	latestPatch := FindLatestPatchVersion(version, versions)
+	patchVersions := []string{"1.6.21", "1.7.0/config.yaml", "1.7.10", "1.7.11-rc1/kyma-installer-cr-cluster.yaml", "1.7.9"}
+	latestPatch := FindLatestPatchVersion(version, patchVersions)
 	require.Equal(t, "1.7.11", latestPatch)
 
 	version = "1.7.9"
-	latestPatch = FindLatestPatchVersion(version, versions)
+	latestPatch = FindLatestPatchVersion(version, patchVersions)
 	require.Equal(t, "1.7.11", latestPatch)
 }
