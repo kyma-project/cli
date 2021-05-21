6f11432eafec1d1e9abff0ab378e507018c00493
Christoph Kleineweber <christoph@kleineweber.de>
2021-05-21 18:34:00 +0200

    Add hint about hosts file if local.kyma.dev not resolvable
    The k3s deployment shoud use *.local.kyma.dev for services that is
    directed to 127.0.0.1. Resolving this domain does not work if DNS rebind
    protection is active. The change detection this situation after
    deploying Kyma and gives a hint to configure an /etc/hosts entry
    alternatively.

--- internal/hosts/domains.go
+++ internal/hosts/domains.go
@@ -12,23 +12,29 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func GetVirtualServiceHostnames(kymaKube kube.KymaKube) ([]string, error) {
+	vsList, err := kymaKube.Istio().NetworkingV1alpha3().VirtualServices("").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	hostnames := []string{}
+	for _, v := range vsList.Items {
+		hostnames = append(hostnames, v.Spec.Hosts...)
+	}
+
+	return hostnames, nil
+}
+
 func AddDevDomainsToEtcHosts(
 	s step.Step, clusterInfo installation.ClusterInfo, kymaKube kube.KymaKube, verbose bool, timeout time.Duration, domain string) error {
-	hostnames := ""
-
-	vsList, err := kymaKube.Istio().NetworkingV1alpha3().VirtualServices("").List(context.Background(), metav1.ListOptions{})
+	vsHostnames, err := GetVirtualServiceHostnames(kymaKube)
 	if err != nil {
 		return err
 	}
+	hostnames := strings.Join(vsHostnames, " ")
 
-	for _, v := range vsList.Items {
-		for _, host := range v.Spec.Hosts {
-			hostnames = hostnames + " " + host
-		}
-	}
-
-	hostAlias := "127.0.0.1" + hostnames
-
+	hostAlias := "127.0.0.1 " + hostnames
 	if clusterInfo.LocalVMDriver != "none" {
 		_, err := minikube.RunCmd(verbose, clusterInfo.Profile, timeout, "ssh", "sudo /bin/sh -c 'echo \""+hostAlias+"\" >> /etc/hosts'")
 		if err != nil {
