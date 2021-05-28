950de0ee30cd50be77d1aee517f3cddb767b1130
Christoph Kleineweber <christoph.kleineweber@sap.com>
2021-05-28 14:16:47 +0200

    Revert refactoring

--- internal/hosts/domains.go
+++ internal/hosts/domains.go
@@ -28,13 +28,21 @@ func GetVirtualServiceHostnames(kymaKube kube.KymaKube) ([]string, error) {
 
 func AddDevDomainsToEtcHosts(
 	s step.Step, clusterInfo installation.ClusterInfo, kymaKube kube.KymaKube, verbose bool, timeout time.Duration, domain string) error {
-	vsHostnames, err := GetVirtualServiceHostnames(kymaKube)
+	hostnames := ""
+
+	vsList, err := kymaKube.Istio().NetworkingV1alpha3().VirtualServices("").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		return err
 	}
-	hostnames := strings.Join(vsHostnames, " ")
 
-	hostAlias := "127.0.0.1 " + hostnames
+	for _, v := range vsList.Items {
+		for _, host := range v.Spec.Hosts {
+			hostnames = hostnames + " " + host
+		}
+	}
+
+	hostAlias := "127.0.0.1" + hostnames
+
 	if clusterInfo.LocalVMDriver != "none" {
 		_, err := minikube.RunCmd(verbose, clusterInfo.Profile, timeout, "ssh", "sudo /bin/sh -c 'echo \""+hostAlias+"\" >> /etc/hosts'")
 		if err != nil {
