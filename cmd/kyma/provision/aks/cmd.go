cef433e81198d36b83a55b5a3d3d18249a7156d6
Adrian Stobbe <adrian.stobbe@sap.com>
2021-07-20 15:31:50 +0200

    bump aks version

--- cmd/kyma/provision/aks/cmd.go
+++ cmd/kyma/provision/aks/cmd.go
@@ -4,6 +4,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var defaultKubernetesVersion string = "1.19.11"
+
 func NewCmd(o *Options) *cobra.Command {
 	c := newAksCmd(o)
 
@@ -19,7 +21,7 @@ func NewCmd(o *Options) *cobra.Command {
 	cmd.Flags().StringVarP(&o.Name, "name", "n", "", "Name of the AKS cluster to provision. (required)")
 	cmd.Flags().StringVarP(&o.Project, "project", "p", "", "Name of the Azure Resource Group where you provision the AKS cluster. (required)")
 	cmd.Flags().StringVarP(&o.CredentialsFile, "credentials", "c", "", "Path to the TOML file containing the Azure Subscription ID (SUBSCRIPTION_ID), Tenant ID (TENANT_ID), Client ID (CLIENT_ID) and Client Secret (CLIENT_SECRET). (required)")
-	cmd.Flags().StringVarP(&o.KubernetesVersion, "kube-version", "k", "1.19.7", "Kubernetes version of the cluster.")
+	cmd.Flags().StringVarP(&o.KubernetesVersion, "kube-version", "k", defaultKubernetesVersion, "Kubernetes version of the cluster.")
 	cmd.Flags().StringVarP(&o.Location, "location", "l", "westeurope", "Region (e.g. westeurope) of the cluster.")
 	cmd.Flags().StringVarP(&o.MachineType, "type", "t", "Standard_D4_v3", "Machine type used for the cluster.")
 	cmd.Flags().IntVar(&o.DiskSizeGB, "disk-size", 50, "Disk size (in GB) of the cluster.")
--- cmd/kyma/provision/aks/cmd_test.go
+++ cmd/kyma/provision/aks/cmd_test.go
@@ -18,7 +18,7 @@ func TestProvisionAKSFlags(t *testing.T) {
 	require.Equal(t, "", o.Name, "Default value for the name flag not as expected.")
 	require.Equal(t, "", o.Project, "Default value for the project flag not as expected.")
 	require.Equal(t, "", o.CredentialsFile, "Default value for the credentials flag not as expected.")
-	require.Equal(t, "1.19.7", o.KubernetesVersion, "Default value for the kube-version flag not as expected.")
+	require.Equal(t, defaultKubernetesVersion, o.KubernetesVersion, "Default value for the kube-version flag not as expected.")
 	require.Equal(t, "westeurope", o.Location, "Default value for the location flag not as expected.")
 	require.Equal(t, "Standard_D4_v3", o.MachineType, "Default value for the type flag not as expected.")
 	require.Equal(t, 50, o.DiskSizeGB, "Default value for the disk-size flag not as expected.")
