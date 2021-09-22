b782479bec50885e5792a386e9345e356d275bb6
pbochynski <p.bochynski@gmail.com>
2021-09-22 14:06:12 +0200

    Change default exposed ports to 80 and 443

--- cmd/kyma/provision/k3d/cmd.go
+++ cmd/kyma/provision/k3d/cmd.go
@@ -2,17 +2,19 @@ package k3d
 
 import (
 	"fmt"
+
 	"github.com/kyma-project/cli/internal/cli"
 	"github.com/kyma-project/cli/internal/clusterinfo"
 	"github.com/kyma-project/cli/internal/k3d"
 	"github.com/kyma-project/cli/internal/kube"
 
-	"github.com/pkg/errors"
-	"github.com/spf13/cobra"
 	"net"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 )
 
 type command struct {
@@ -43,7 +45,7 @@ func NewCmd(o *Options) *cobra.Command {
 	cmd.Flags().DurationVar(&o.Timeout, "timeout", 5*time.Minute, `Maximum time for the provisioning. If you want no timeout, enter "0".`)
 	cmd.Flags().StringSliceVarP(&o.K3dArgs, "k3d-arg", "", []string{}, "One or more arguments passed to the k3d provisioning command (e.g. --k3d-arg='--no-rollback')")
 	cmd.Flags().StringVarP(&o.KubernetesVersion, "kube-version", "k", "1.20.7", "Kubernetes version of the cluster")
-	cmd.Flags().StringSliceVarP(&o.PortMapping, "port", "p", []string{"8000:80@loadbalancer", "8443:443@loadbalancer"}, "Map ports 80 and 443 of K3D loadbalancer (e.g. -p 8000:80@loadbalancer -p 8443:443@loadbalancer)")
+	cmd.Flags().StringSliceVarP(&o.PortMapping, "port", "p", []string{"80:80@loadbalancer", "443:443@loadbalancer"}, "Map ports 80 and 443 of K3D loadbalancer (e.g. -p 8000:80@loadbalancer -p 8443:443@loadbalancer)")
 	return cmd
 }
 
