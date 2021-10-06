e8ecc1b3294b5efbf841abaaf92aed254c256f1a
Benjamin Lindner <benjamin.lindner@sap.com>
2021-10-06 11:10:40 +0200

    Increase timeout and provide flag for setting it

--- cmd/kyma/undeploy/opts.go
+++ cmd/kyma/undeploy/opts.go
@@ -2,12 +2,14 @@ package undeploy
 
 import (
 	"github.com/kyma-project/cli/internal/cli"
+	"time"
 )
 
 //Options defines available options for the command
 type Options struct {
 	*cli.Options
 	KeepCRDs bool
+	Timeout time.Duration
 }
 
 //NewOptions creates options with default values
