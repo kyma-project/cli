dbbfed405974d122da06066eefcd071748af03b0
Dennis Geiselmann <dennis.geiselmann@outlook.de>
2021-11-04 08:39:58 +0100

    add empty line

--- cmd/kyma/dashboard/cmd.go
+++ cmd/kyma/dashboard/cmd.go
@@ -120,6 +120,7 @@ func (cmd *command) runDashboardContainer(dashboardURL, kubeconfigPath, kubeconf
 		cmd.Finalizers.Add(dockerWrapper.Stop(followCtx, id, func(i ...interface{}) { fmt.Print(i...) }))
 		return dockerWrapper.ContainerFollowRun(followCtx, id)
 	}
+
 	return nil
 }
 
