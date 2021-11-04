abe52df9d0580493379e52a58924289410650162
Christoph Kleineweber <c.kleineweber@sap.com>
2021-11-04 11:56:55 +0100

    Fix import hosts command
    - Do not import hosts if already existing
    - Use istio-ingressgateway external IP if not on local dev cluster

--- internal/hosts/domains.go
+++ internal/hosts/domains.go
@@ -1,7 +1,9 @@
 package hosts
 
 import (
+	"bufio"
 	"context"
+	"os"
 	"strings"
 	"time"
 
@@ -10,6 +12,10 @@ import (
 	"github.com/kyma-project/cli/pkg/installation"
 	"github.com/kyma-project/cli/pkg/step"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	localDevDomain = "local.kyma.dev"
 )
 
 func GetVirtualServiceHostnames(kymaKube kube.KymaKube) ([]string, error) {
@@ -54,8 +60,8 @@ func AddDevDomainsToEtcHosts(
 
 	return addDevDomainsToEtcHostsOSSpecific(domain, s, hostAlias)
 }
-func AddDevDomainsToEtcHostsKyma2(
-	s step.Step, kymaKube kube.KymaKube, domain string) error {
+
+func AddDevDomainsToEtcHostsKyma2(s step.Step, kymaKube kube.KymaKube) error {
 	hostnames := ""
 
 	vsList, err := kymaKube.Istio().NetworkingV1alpha3().VirtualServices("").List(context.Background(), metav1.ListOptions{})
@@ -69,6 +75,61 @@ func AddDevDomainsToEtcHostsKyma2(
 		}
 	}
 
-	hostAlias := "127.0.0.1" + hostnames
-	return addDevDomainsToEtcHostsOSSpecificKyma2(domain, s, hostAlias)
+	var gatewayAddress string
+	if strings.Contains(hostnames, localDevDomain) {
+		gatewayAddress = "127.0.0.1"
+	} else {
+		gatewayAddress, err = getIngressGatewayAddress(kymaKube)
+		if err != nil {
+			return err
+		}
+	}
+	hostAlias := gatewayAddress + hostnames
+
+	existingEntry, err := hasHostsEntry(hostAlias)
+	if err != nil {
+		return err
+	}
+	if existingEntry {
+		s.LogInfof("Domain mappings are already existing in your 'hosts' file")
+		return nil
+	}
+
+	return writeHostsEntry(hostAlias)
+}
+
+func getIngressGatewayAddress(kymaKube kube.KymaKube) (string, error) {
+	svc, err := kymaKube.Static().CoreV1().Services("istio-system").Get(context.Background(), "istio-ingressgateway", metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	return svc.Status.LoadBalancer.Ingress[0].IP, nil
+}
+
+func hasHostsEntry(hostAlias string) (bool, error) {
+	f, err := os.Open(hostsFile)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	fileScanner := bufio.NewScanner(f)
+	for fileScanner.Scan() {
+		if fileScanner.Text() == hostAlias {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func writeHostsEntry(hostAlias string) error {
+	f, err := os.OpenFile(hostsFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(hostAlias + "\n")
+	return err
 }
