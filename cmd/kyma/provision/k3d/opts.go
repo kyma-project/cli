481966420f6e0fcc3881deb4818c48db322f6693
Benjamin Lindner <50365642+lindnerby@users.noreply.github.com>
2021-11-29 19:19:21 +0100

    Move alpha provision k3d impl to stable cmd (#1095)
    * Move alpha provision k3d impl to stable cmd
    
    * Make docs
    
    * Update impl from alpha
    
    * Make docs

--- cmd/kyma/provision/k3d/opts.go
+++ cmd/kyma/provision/k3d/opts.go
@@ -13,8 +13,9 @@ type Options struct {
 	Name              string
 	Workers           int
 	Timeout           time.Duration
-	ServerArgs        []string
-	AgentArgs         []string
+	K3sArgs           []string
+	UseRegistry       []string
+	RegistryPort      string
 	K3dArgs           []string
 	KubernetesVersion string
 	PortMapping       []string
