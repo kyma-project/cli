b95f0d05684c86fe687d95be09d4540e4f16aa41
Benjamin Lindner <benjamin.lindner@sap.com>
2022-02-15 09:27:44 +0100

    Add more tests for parsing component strings

--- internal/deploy/component/list_test.go
+++ internal/deploy/component/list_test.go
@@ -74,4 +74,39 @@ func TestFromStrings(t *testing.T) {
 		require.Equal(t, "comp-4", list.Components[3].Name)
 		require.Equal(t, "ns-2", list.Components[3].Namespace)
 	})
+
+	t.Run("Add Component via JSON format notation", func(t *testing.T) {
+		list, _ := FromStrings([]string{"{\"name\": \"comp-1\",\"namespace\": \"ns-1\",\"url\": \"some-url\",\"version\": \"1.2.3\"}"})
+		require.Equal(t, "kyma-system", list.DefaultNamespace)
+		require.Equal(t, "comp-1", list.Components[0].Name)
+		require.Equal(t, "ns-1", list.Components[0].Namespace)
+		require.Equal(t, "some-url", list.Components[0].URL)
+		require.Equal(t, "1.2.3", list.Components[0].Version)
+	})
+
+	t.Run("Add Component via JSON format notation with default namespace and no version", func(t *testing.T) {
+		list, _ := FromStrings([]string{"{\"name\": \"comp-1\",\"url\": \"some-url\"}"})
+		require.Equal(t, "kyma-system", list.DefaultNamespace)
+		require.Equal(t, "comp-1", list.Components[0].Name)
+		require.Equal(t, "kyma-system", list.Components[0].Namespace)
+		require.Equal(t, "some-url", list.Components[0].URL)
+		require.Empty(t, list.Components[0].Version)
+	})
+
+	t.Run("Add multiple Components via different notations", func(t *testing.T) {
+		list, _ := FromStrings([]string{"comp-1@ns-1", "{\"name\": \"comp-2\",\"url\": \"some-url\"}", "comp-3", "{\"name\": \"comp-4\",\"namespace\": \"ns-2\",\"url\": \"some-url-2\",\"version\": \"3.2\"}"})
+		require.Equal(t, "kyma-system", list.DefaultNamespace)
+		require.Equal(t, "comp-1", list.Components[0].Name)
+		require.Equal(t, "ns-1", list.Components[0].Namespace)
+		require.Equal(t, "comp-2", list.Components[1].Name)
+		require.Equal(t, "kyma-system", list.Components[1].Namespace)
+		require.Equal(t, "some-url", list.Components[1].URL)
+		require.Empty(t, list.Components[1].Version)
+		require.Equal(t, "comp-3", list.Components[2].Name)
+		require.Equal(t, "kyma-system", list.Components[2].Namespace)
+		require.Equal(t, "comp-4", list.Components[3].Name)
+		require.Equal(t, "ns-2", list.Components[3].Namespace)
+		require.Equal(t, "some-url-2", list.Components[3].URL)
+		require.Equal(t, "3.2", list.Components[3].Version)
+	})
 }
