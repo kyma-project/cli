e91443cb6cb62b9f2d4b064f8112f6f1556b4af3
Jeremy Harisch <jeremy.harisch@sap.com>
2022-03-25 13:03:50 +0100

    lint

--- cmd/kyma/deploy/cmd.go
+++ cmd/kyma/deploy/cmd.go
@@ -55,12 +55,12 @@ func NewCmd(o *Options) *cobra.Command {
 	- With verbose JSON structure "--component '{"name": "componentName","namespace": "componenNamespace","url": "componentUrl","version": "1.2.3"}'`)
 	cobraCmd.Flags().StringVarP(&o.ComponentsFile, "components-file", "c", "", `Path to the components file (default "$HOME/.kyma/sources/installation/resources/components.yaml" or ".kyma-sources/installation/resources/components.yaml")`)
 	cobraCmd.Flags().StringVarP(&o.WorkspacePath, "workspace", "w", "", `Path to download Kyma sources (default "$HOME/.kyma/sources" or ".kyma-sources")`)
-	cobraCmd.Flags().StringVarP(&o.Source, "source", "s", config.DefaultKyma2Version, fmt.Sprint(`Installation source:
+	cobraCmd.Flags().StringVarP(&o.Source, "source", "s", config.DefaultKyma2Version,`Installation source:
 	- Deploy a specific release, for example: "kyma deploy --source=2.0.0"
 	- Deploy a specific branch of the Kyma repository on kyma-project.org: "kyma deploy --source=<my-branch-name>"
 	- Deploy a commit (8 characters or more), for example: "kyma deploy --source=34edf09a"
 	- Deploy a pull request, for example "kyma deploy --source=PR-9486"
-	- Deploy the local sources: "kyma deploy --source=local"`))
+	- Deploy the local sources: "kyma deploy --source=local"`)
 	cobraCmd.Flags().StringVarP(&o.Domain, "domain", "d", "", "Custom domain used for installation.")
 	cobraCmd.Flags().BoolVar(&o.DryRun, "dry-run", false, "Alpha feature: Renders the Kubernetes manifests without actually applying them. The generated resources are not sufficient to apply Kyma to a cluster, because components having custom installation routines (such as Istio) are not included.")
 	cobraCmd.Flags().StringVarP(&o.Profile, "profile", "p", "",
