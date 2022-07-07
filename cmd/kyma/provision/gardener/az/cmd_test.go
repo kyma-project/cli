01d211f45086ca9b2cf109d9e479ffa990434eb7
Andreas Thaler <andreas.thaler01@sap.com>
2022-07-07 10:52:58 +0200

    update to k8s 1.23

--- cmd/kyma/provision/gardener/az/cmd_test.go
+++ cmd/kyma/provision/gardener/az/cmd_test.go
@@ -19,7 +19,7 @@ func TestProvisionGardenerAzureFlags(t *testing.T) {
 	require.Equal(t, "", o.Project, "Default value for the project flag not as expected.")
 	require.Equal(t, "", o.CredentialsFile, "Default value for the credentials flag not as expected.")
 	require.Equal(t, "", o.Secret, "The parsed value for the secret flag not as expected.")
-	require.Equal(t, "1.21", o.KubernetesVersion, "Default value for the kube-version flag not as expected.")
+	require.Equal(t, "1.23", o.KubernetesVersion, "Default value for the kube-version flag not as expected.")
 	require.Equal(t, "westeurope", o.Region, "Default value for the region flag not as expected.")
 	require.Equal(t, []string{"1"}, o.Zones, "Default value for the zone flag not as expected.")
 	require.Equal(t, "Standard_D4_v3", o.MachineType, "Default value for the type flag not as expected.")
