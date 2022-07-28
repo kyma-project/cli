129fabba14b4f5d051861b5f094483b4b4ee8c0f
Xin Ruan <ruanxin1@gmail.com>
2022-07-28 16:11:35 +0200

    Update cmd/kyma/alpha/sign/sign.go

--- cmd/kyma/alpha/sign/sign.go
+++ cmd/kyma/alpha/sign/sign.go
@@ -11,7 +11,7 @@ func NewCmd(o *cli.Options) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "sign",
 		Short: "Sign all module resources from unsigned component descriptor which hosted in a remote OCI registry",
-		Long:  "Sign all module resources from unsigned component descriptor which hosted in a remote OCI registry",
+		"Use this command to sign all module resources from an unsigned component descriptor that's hosted in a remote OCI registry",
 	}
 
 	cmd.AddCommand(module.NewCmd(module.NewOptions(o)))
