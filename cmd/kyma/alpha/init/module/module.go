239646772a7767864a066b00628e64d92eba3022
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2022-08-04 12:15:59 +0200

    Language review fixes
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>

--- cmd/kyma/alpha/init/module/module.go
+++ cmd/kyma/alpha/init/module/module.go
@@ -34,16 +34,16 @@ Kyma modules are individual components that can be deployed into a Kyma runtime.
 With this command, you can initialize an empty module folder for the purpose of further development.
 
 This command creates a module directory in the current working directory.
+To create the module directory at a different location, use the "--path" flag.
 The name of the  module directory is the same as the name of the module.
 Module name must start with a letter and may only consist of alphanumeric and '.', '_', or '-' characters.
-You may optionally provide a "--path" flag to create module directory at some different location.
-In the module directory you'll find the template files and subdirectories corresponding to the required module structure:
+In the module directory, you'll find the template files and subdirectories corresponding to the required module structure:
     charts/       // folder containing a set of charts (each in a subfolder)
     crds/         // folder containing all CRDs required by the module
     operator/     // folder containing the operator needed to manage the module
     profiles/     // folder containing all profile settings
     channels/     // folder containing all channel settings
-    config.yaml   // YAML file containing installation configuration for any chart in the module that requires custom Helm settings
+    config.yaml   // YAML file containing the installation configuration for any chart in the module that requires custom Helm settings
     README.md     // document explaining the module format, how it translates to OCI images, and how to develop one (can be mostly empty at the beginning)
 `,
 
