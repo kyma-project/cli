f701fbc468ea9a39a5435bb72abecb70fd966b5a
Borja Clemente <clebs@users.noreply.github.com>
2022-09-22 12:28:10 +0200

    Start release 2.7. (#1384)
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>
    
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,7 +6,7 @@
 package config
 
 const (
-	DefaultKyma2Version = "main"
+	DefaultKyma2Version = "2.7.0"
 	// DefaultManagerVersion sets the version for the kyma lifecycle and module managers to install when deploying the kyma setup.
 	DefaultManagerVersion = "main"
 )
