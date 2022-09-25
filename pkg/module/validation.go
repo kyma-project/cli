ee38340672b67ae8a905866a073a772f4731a14f
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2022-09-26 01:53:53 +0200

    Given group/kind find the proper CRD to start envtest

--- pkg/module/validation.go
+++ pkg/module/validation.go
@@ -3,6 +3,7 @@ package module
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -18,7 +19,70 @@ import (
 
 const defaultCRName = "default.yaml"
 
-//readDefaultCR reads the default CR file's contents. The first returned value is true if the file exists. It's false if the file does not exist or an error occurred.
+func ValidateDefaultCR(modulePath string, log *zap.SugaredLogger) (skipped bool, err error) {
+
+	exists, crData, err := readDefaultCR(modulePath)
+	if err != nil {
+		return false, err
+	}
+	if !exists {
+		//If there's no "default.yaml" file, skip the validation.
+		return true, nil
+	}
+
+	crMap, err := parseYamlToMap(crData)
+	if err != nil {
+		return false, fmt.Errorf("Error during parsing default CR: %w", err)
+	}
+
+	group, kind, err := readGroupKind(crMap)
+	if err != nil {
+		return false, err
+	}
+
+	searchDirPath := filepath.Join(modulePath, "charts")
+	crdFound, crdFilePath, err := findCRDFileFor(group, kind, searchDirPath)
+	if err != nil {
+		return false, fmt.Errorf("Error finding CRD file in the %q directory: %w", searchDirPath, err)
+	}
+	if !crdFound {
+		return false, fmt.Errorf("Can't find the CRD for CR instance defined in \"default.yaml\" (group: %q, kind %q)", group, kind)
+	}
+
+	err = ensureDefaultNamespace(crMap)
+	if err != nil {
+		return false, err
+	}
+
+	properCR, err := renderYamlFromMap(crMap)
+	if err != nil {
+		return false, err
+	}
+
+	envTest, restCfg, err := startValidationEnv(crdFilePath)
+	if err != nil {
+		return false, err
+	}
+
+	defer func() {
+		stopErr := envTest.Stop()
+		if stopErr != nil {
+			//TODO: This doesn't seem to print anything...
+			log.Error(fmt.Errorf("Error during stopping envTest: %w", stopErr))
+			//THIS does: fmt.Println(fmt.Errorf("Error during stopping envTest: %w", stopErr))
+		}
+	}()
+
+	kc, err := kube.NewFromRestConfigWithTimeout(restCfg, 30*time.Second)
+
+	err = kc.Apply(properCR)
+	if err != nil {
+		return false, fmt.Errorf("Error during applying the default CR: %w", err)
+	}
+	return false, nil
+}
+
+//readDefaultCR reads the default CR file's contents. The returned bool is true if the data is successfully read, it's false otherwise.
 func readDefaultCR(modulePath string) (bool, []byte, error) {
 	//TODO: Do we need to override the name or it's always "default.yaml"?
 	crPath := filepath.Join(modulePath, defaultCRName)
@@ -43,25 +107,13 @@ func readDefaultCR(modulePath string) (bool, []byte, error) {
 	return true, crData, nil
 }
 
-//ensureDefaultNamespace parses crData and ensures that metadata.namespace is set to the value "default". This is because of how we use the envtest to validate the CR.
-func ensureDefaultNamespace(crData []byte) ([]byte, error) {
-
-	//Parse input data into a generic Map
-	modelMap := make(map[string]interface{})
-	err := yaml.Unmarshal([]byte(crData), &modelMap)
-	if err != nil {
-		return nil, fmt.Errorf("Error during parsing default CR: %w", err)
-	}
+//ensureDefaultNamespace ensures that metadata.namespace is set to the value "default". This is because of how we use the envtest to validate the CR.
+func ensureDefaultNamespace(modelMap map[string]interface{}) error {
 
 	//Traverse the Map to look for "metadata.namespace"
-	metadataVal, ok := modelMap["metadata"]
-	if !ok {
-		return nil, errors.New("Error during parsing default CR: no \"metadata\" attribute")
-	}
-
-	metadataMap, ok := metadataVal.(map[string]interface{})
-	if !ok {
-		return nil, errors.New("Error during parsing default CR: \"metadata\" attribute is not a Map")
+	metadataMap, err := mustReadMap(modelMap, "metadata")
+	if err != nil {
+		return fmt.Errorf("Error during parsing default CR: %w", err)
 	}
 
 	namespaceVal, ok := metadataMap["namespace"]
@@ -72,67 +124,90 @@ func ensureDefaultNamespace(crData []byte) ([]byte, error) {
 		//Set the "metadata.namespace" if different than "default"
 		existing, ok := namespaceVal.(string)
 		if !ok {
-			return nil, errors.New("Error during parsing default CR: \"metadata.namespace\" attribute is not a string")
+			return errors.New("Error during parsing default CR: Attribute \"metadata.namespace\" is not a string")
 		}
 		if existing != "default" {
 			metadataMap["namespace"] = "default"
 		}
 	}
 
-	output, err := yaml.Marshal(modelMap)
+	return nil
+}
+
+func readGroupKind(crMap map[string]interface{}) (group, kind string, retErr error) {
+	apiVersion, err := mustReadString(crMap, "apiVersion")
+	if err != nil {
+		retErr = fmt.Errorf("Can't parse default CR data: %w", err)
+		return
+	}
+
+	group = strings.Split(apiVersion, "/")[0] //e.g: apiVersion: example.org/v1
+	kind, err = mustReadString(crMap, "kind")
+	if err != nil {
+		retErr = fmt.Errorf("Can't parse default CR data: %w", err)
+		return
+	}
+
+	return
+}
+
+//mustReadMap reads a value from the given map using the given key. The value must be a Map. An error is returned if the key is not in the input map, or the value is not a Map.
+func mustReadMap(input map[string]interface{}, key string) (map[string]interface{}, error) {
+	attrVal, ok := input[key]
+	if !ok {
+		return nil, fmt.Errorf("Attribute %q not found", key)
+	}
+
+	asMap, ok := attrVal.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("Attribute %q is not a Map", key)
+	}
+
+	return asMap, nil
+}
+
+//mustReadString reads a value from the given map using the given key. The value must be a string. An error is returned if the key is not in the map, or the value is not a string.
+func mustReadString(input map[string]interface{}, key string) (string, error) {
+	attrVal, ok := input[key]
+	if !ok {
+		return "", fmt.Errorf("Attribute %q not found", key)
+	}
+
+	asString, ok := attrVal.(string)
+	if !ok {
+		return "", fmt.Errorf("Attribute %q is not a string", key)
+	}
+
+	return asString, nil
+}
+
+func parseYamlToMap(crData []byte) (map[string]interface{}, error) {
+	modelMap := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(crData), &modelMap)
 	if err != nil {
 		return nil, fmt.Errorf("Error during parsing default CR: %w", err)
 	}
 
+	return modelMap, nil
+}
+
+func renderYamlFromMap(modelMap map[string]interface{}) ([]byte, error) {
+
+	output, err := yaml.Marshal(modelMap)
+	if err != nil {
+		return nil, fmt.Errorf("Error during processing default CR data: %w", err)
+	}
+
 	return output, nil
+
 }
 
-func ValidateDefaultCR(modulePath string, log *zap.SugaredLogger) (skipped bool, err error) {
-
-	exists, rawCRData, err := readDefaultCR(modulePath)
-	if err != nil {
-		return false, err
-	}
-	if !exists {
-		//If there's no "default.yaml" file, skip the validation.
-		return true, nil
-	}
-
-	crData, err := ensureDefaultNamespace(rawCRData)
-	if err != nil {
-		return false, err
-	}
-
-	//TODO: How to get this directory path programatically? What's the contract?
-	crdDirectoryPath := os.Getenv("CRD_DIRECTORY")
-
-	envTest, restCfg, err := startValidationEnv(crdDirectoryPath)
-	if err != nil {
-		return false, err
-	}
-
-	defer func() {
-		stopErr := envTest.Stop()
-		if stopErr != nil {
-			//TODO: This doesn't seem to print anything...
-			log.Error(fmt.Errorf("Error during stopping envTest: %w", stopErr))
-			//THIS does: fmt.Println(fmt.Errorf("Error during stopping envTest: %w", stopErr))
-		}
-	}()
-
-	kc, err := kube.NewFromRestConfigWithTimeout(restCfg, 30*time.Second)
-
-	err = kc.Apply(crData)
-	if err != nil {
-		return false, fmt.Errorf("Error during applying the default CR: %w", err)
-	}
-	return false, nil
-}
-
-func startValidationEnv(crdDirectoryPath string) (*envtest.Environment, *rest.Config, error) {
+func startValidationEnv(crdFilePath string) (*envtest.Environment, *rest.Config, error) {
 
 	envTest := &envtest.Environment{
-		CRDDirectoryPaths:     []string{crdDirectoryPath},
+		CRDInstallOptions: envtest.CRDInstallOptions{
+			Paths: []string{crdFilePath},
+		},
 		ErrorIfCRDPathMissing: true,
 	}
 
@@ -145,6 +220,128 @@ func startValidationEnv(crdDirectoryPath string) (*envtest.Environment, *rest.Co
 	}
 
 	return envTest, restCfg, nil
+}
+
+//findCRDFileFor returns path to the file with a CRD definition for the given group and kind, if exists. It looks in the dirPath directory and all of it's subdirectories, recursively. The first parameter is true if the file is found, it's false otherwise.
+func findCRDFileFor(group, kind, dirPath string) (bool, string, error) {
+
+	//list all files in the dirPath and all it's subdirectories, recursively
+	files, err := listFiles(dirPath)
+	if err != nil {
+		return false, "", fmt.Errorf("Error listing files in %q directory: %w", dirPath, err)
+	}
+
+	var found string
+	for _, f := range files {
+		//fmt.Printf("- Checking file: %q\n", f)
+		ok, err := isCRDFileFor(group, kind, f)
+		if err != nil {
+			//Error is expected. Either the file is not YAML, or it's not a CRD, or it's a CRD but not the one we're looking for.
+			continue
+		}
+		if ok {
+			found = f
+			break
+		}
+	}
+
+	if found != "" {
+		return true, found, nil
+	}
+
+	return false, "", nil
+}
+
+//isCRDFileFor checks if the given file is a CRD for given group and kind.
+func isCRDFileFor(group, kind, filePath string) (bool, error) {
+	{
+
+		f, err := os.Open(filePath)
+		if err != nil {
+			return false, fmt.Errorf("Error reading \"%q\": %w", filePath, err)
+		}
+		defer f.Close()
+
+		yd := yaml.NewDecoder(f)
+
+		isNotEOF := func(err error) bool {
+			return !errors.Is(err, io.EOF)
+		}
+
+		//Iteration is necessary, because the file may contain several documents and the CRD we're looking for may not be the first one.
+		err = nil
+		for err == nil || isNotEOF(err) {
+			modelMap := make(map[string]interface{})
+			err = yd.Decode(modelMap)
+			if err != nil {
+				//fail fast if it's not a proper YAML
+				return false, err
+			}
+
+			//Ensure it's a CRD
+			rootKindVal, err := mustReadString(modelMap, "kind")
+			if err != nil {
+				continue
+			}
+			if rootKindVal != "CustomResourceDefinition" {
+				continue
+			}
+
+			//Find the Group/Kind of this CRD
+			specMap, err := mustReadMap(modelMap, "spec")
+			if err != nil {
+				continue
+			}
+			groupVal, err := mustReadString(specMap, "group")
+			if err != nil {
+				continue
+			}
+			namesMap, err := mustReadMap(specMap, "names")
+			if err != nil {
+				continue
+			}
+			kindVal, err := mustReadString(namesMap, "kind")
+			if err != nil {
+				continue
+			}
+
+			//Check if this CRD is the one we're looking for
+			if groupVal == group && kindVal == kind {
+				return true, nil
+			}
+		}
+
+		if errors.Is(err, io.EOF) {
+			//Failure: No document in the file matches our search criteria.
+			return false, nil
+		}
+
+		//We should never get here, because Decode() should return EOF once there's no more data to read.
+		return false, err
+	}
+}
+
+//listFiles returns an unordered slice of all the files within the given directory and all it's subdirectories, recursively.
+func listFiles(dirPath string) ([]string, error) {
+
+	res := []string{}
+
+	walkFunc := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.Mode().IsRegular() {
+			res = append(res, path)
+		}
+		return nil
+	}
+
+	err := filepath.Walk(dirPath, walkFunc)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading directory \"%q\": %w", dirPath, err)
+	}
+
+	return res, nil
 }
 
 // ValidateName checks if the name is at least three characters long and if it conforms to the "RFC 1035 Label Names" specification (K8s compatibility requirement)
--- pkg/module/validation_test.go
+++ pkg/module/validation_test.go
@@ -13,24 +13,21 @@ func TestEnsureDefaultNamespace(t *testing.T) {
 		outputVal          string
 		errorShouldContain string
 	}{
+
 		{
 			name:      "happy path",
 			input:     correctModel,
 			shouldErr: false,
 			outputVal: correctModel,
 		},
-		{
-			name:               "invalid YAML",
-			input:              invalidYaml,
-			shouldErr:          true,
-			errorShouldContain: InvalidYamlError,
-		},
+
 		{
 			name:               "missing metadata",
 			input:              noMetadataYaml,
 			shouldErr:          true,
 			errorShouldContain: noMetadataError,
 		},
+
 		{
 			name:               "metadata is not map",
 			input:              invalidMetadataTypeYaml,
@@ -59,18 +56,34 @@ func TestEnsureDefaultNamespace(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			output, err := ensureDefaultNamespace([]byte(tc.input))
+			crData, err := parseYamlToMap([]byte(tc.input))
+			if err != nil {
+				t.Fatalf("Can't parse test data: %v", err)
+				return
+			}
+
+			err = ensureDefaultNamespace(crData)
+
 			if err == nil && tc.shouldErr {
 				t.Errorf("ensureDefaultNamespace() should return an error but it didn't")
+				return
 			}
 			if err != nil && !tc.shouldErr {
 				t.Errorf("ensureDefaultNamespace() should not return an error but it did: %v", err)
+				return
 			}
 			if err != nil && !strings.Contains(err.Error(), tc.errorShouldContain) {
 				t.Errorf("ensureDefaultNamespace() should return an error containing: \"%v\"\nbut it returned: %v", tc.errorShouldContain, err.Error())
+				return
+			}
+
+			output, yamlErr := renderYamlFromMap(crData)
+			if yamlErr != nil {
+				t.Fatalf("Can't render test data: %v", yamlErr)
 			}
 			if err == nil && string(output) != tc.outputVal {
 				t.Errorf("ensureDefaultNamespace() should return:\n%v\nbut it returned:\n%v", tc.outputVal, string(output))
+				return
 			}
 		})
 
@@ -86,26 +99,24 @@ metadata:
 spec:
     releaseName: redis-release
 `
-	invalidYaml      = `!abc: def`
-	InvalidYamlError = "cannot unmarshal !abc: `def` into map[string]interface {}"
 
 	noMetadataYaml = `abc: def
 foo: bar
 `
-	noMetadataError = `no "metadata" attribute`
+	noMetadataError = `Attribute "metadata" not found`
 
 	invalidMetadataTypeYaml = `
 abc: def
 metadata: 2
 `
-	invalidMetadataTypeError = `"metadata" attribute is not a Map`
+	invalidMetadataTypeError = `Attribute "metadata" is not a Map`
 
 	invalidNamespaceTypeYaml = `
 abc: def
 metadata:
   namespace: 2
 `
-	invalidNamespaceTypeError = `"metadata.namespace" attribute is not a string`
+	invalidNamespaceTypeError = `Attribute "metadata.namespace" is not a string`
 	missingNamespaceYaml      = `abc: def
 metadata:
     name: foobar
