24ae3d30f54456696bbd399e3b8a4d0ccc92fc77
Amritanshu Sikdar <amritanshu.sikdar@sap.com>
2023-03-29 12:26:52 +0200

    refactor version checker to its own new file

---
+++ cmd/kyma/version/checker.go
@@ -0,0 +1,67 @@
+package version
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"runtime/debug"
+)
+
+type KymaCLIMetadata struct {
+	Name   string `json:"name"`
+	Commit struct {
+		SHA string `json:"sha"`
+	} `json:"commit"`
+}
+
+// CheckForStableRelease Checks for new versions of the CLI
+func CheckForStableRelease() {
+	response, err := http.Get("https://api.github.com/repos/kyma-project/cli/tags")
+
+	//	For any problems in fetching, reading or parsing the response from GitHub API, we simply ignore it
+	//	and don't disrupt the usual CLI Flow
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+
+	var githubTags []KymaCLIMetadata
+	err = json.Unmarshal(responseData, &githubTags)
+	if err != nil {
+		return
+	}
+	var stableCLI KymaCLIMetadata
+	for _, tag := range githubTags {
+		if tag.Name == "stable" {
+			stableCLI = tag
+			break
+		}
+	}
+
+	var stableCLINumber string
+	for _, tag := range githubTags {
+		if tag.Name != stableCLI.Name && tag.Commit.SHA == stableCLI.Commit.SHA {
+			stableCLINumber = tag.Name
+		}
+	}
+
+	var currentCommitSHA string
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				currentCommitSHA = setting.Value
+			}
+		}
+	}
+
+	if Version != stableCLINumber || currentCommitSHA != stableCLI.Commit.SHA {
+		fmt.Println("CAUTION: You're using an outdated version of the Kyma CLI. The latest stable version is: ", stableCLINumber)
+		fmt.Println()
+	}
+}
