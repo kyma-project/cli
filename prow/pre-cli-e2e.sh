986689fabaea5f3545df784580b2ad246b705477
Badr, Nesma <nesma.badr@sap.com>
2023-07-20 11:07:21 +0200

    add todo comment

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -6,5 +6,5 @@ cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
 kyma deploy --ci
 make -C "../kyma/tests/fast-integration" "ci"
-#Uncomment when https://github.com/kyma-project/cli/issues/1701 is done
+#TODO: Uncomment when https://github.com/kyma-project/cli/issues/1701 is done
 #kyma undeploy --ci --timeout=10m0s
