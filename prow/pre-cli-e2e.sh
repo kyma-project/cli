8e48a73e4ee9764f8611961437873c142aaa9718
Badr, Nesma <nesma.badr@sap.com>
2023-07-20 10:25:48 +0200

    edit script

---
+++ prow/pre-cli-e2e.sh
@@ -0,0 +1,6 @@
+make build-linux
+cp ./bin/kyma-linux /usr/local/bin/kyma
+kyma provision k3d --ci
+kyma deploy --ci
+make -C "../kyma/tests/fast-integration" "ci"
+kyma undeploy --ci --timeout=10m0s
