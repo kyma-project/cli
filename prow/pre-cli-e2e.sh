02394805afa2eb78f0fc9002b437b1c49be5d497
Badr, Nesma <nesma.badr@sap.com>
2023-07-25 22:01:11 +0200

    revert timeout change

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -5,5 +5,5 @@ make build-linux
 cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
 kyma deploy --ci
-make -C "../kyma/tests/fast-integration" "ci"
-kyma undeploy --ci --timeout=20m0s --verbose
+#make -C "../kyma/tests/fast-integration" "ci"
+kyma undeploy --ci --timeout=10m0s --verbose
