12983591ca48891ccbe279639a4bedc61df623c3
Badr, Nesma <nesma.badr@sap.com>
2023-07-25 21:33:21 +0200

    increasing timeout

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -6,4 +6,4 @@ cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
 kyma deploy --ci
 make -C "../kyma/tests/fast-integration" "ci"
-kyma undeploy --ci --timeout=15m0s --verbose
+kyma undeploy --ci --timeout=20m0s --verbose
