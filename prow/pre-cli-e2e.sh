df28d6872dfdd238aac3fb557652dc011364bd42
Badr, Nesma <nesma.badr@sap.com>
2023-07-25 13:24:24 +0200

    Debugging scriptgit

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -4,14 +4,10 @@ make resolve
 make build-linux
 cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
-echo "BEFORE--/root/.kyma/sources/main"
-ls /root/.kyma/sources/main
 echo "BEFORE--cat"
 cat $HOME/.kyma/sources/installation/resources/components.yaml
 kyma deploy --ci
 make -C "../kyma/tests/fast-integration" "ci"
-echo "AFTER--/root/.kyma/sources/main"
-ls /root/.kyma/sources/main
 echo "AFTER--cat"
 cat $HOME/.kyma/sources/installation/resources/components.yaml
 kyma undeploy --ci --timeout=10m0s
