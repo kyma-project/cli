241eadec50092ed3a78df15590c77878580f6194
Badr, Nesma <nesma.badr@sap.com>
2023-07-25 18:33:34 +0200

    rerun pipeline

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -6,4 +6,4 @@ cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
 kyma deploy --ci
 make -C "../kyma/tests/fast-integration" "ci"
-kyma undeploy --ci --timeout=15m0s
+kyma undeploy --ci --timeout=15m0s --verbose
