585f3f455127e275d4b07b8f39c2253ee32b7017
Badr, Nesma <nesma.badr@sap.com>
2023-07-25 22:26:03 +0200

    add sleep after integration tests

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -5,5 +5,6 @@ make build-linux
 cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
 kyma deploy --ci
-#make -C "../kyma/tests/fast-integration" "ci"
-kyma undeploy --ci --timeout=10m0s --verbose
+make -C "../kyma/tests/fast-integration" "ci"
+sleep 10
+kyma undeploy --ci --timeout=10m0s
