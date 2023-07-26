3ed263918918d1c1d44d3bcd71719a598002e5db
Badr, Nesma <nesma.badr@sap.com>
2023-07-26 14:30:46 +0200

    remove fast integration tests

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -1,9 +1,7 @@
 set -e
-apk add nodejs npm
 make resolve
 make build-linux
 cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
 kyma deploy --ci
-make -C "../kyma/tests/fast-integration" "ci"
 kyma undeploy --ci --timeout=10m0s
