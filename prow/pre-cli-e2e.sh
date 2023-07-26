3719ca85bbfd7591cc638c7aef6e9cb1ce0b0255
Nesma Badr <nesmabadr@aucegypt.edu>
2023-07-26 15:32:35 +0200

    Add kyma undeploy command to pre submit job (#1711)
    * remove script + add make targets
    
    * fix make target
    
    * fix
    
    * remove new target
    
    * Revert "remove script + add make targets"
    
    This reverts commit a7681053b036f05cc760013a6e6a6050715658bd.
    
    * edit script
    
    * edit script
    
    * remove wrong log
    
    * comment undeployment from script
    
    * add todo comment
    
    * Uncomment kyma undeploy from script
    
    * Uncomment kyma undeploy from script
    
    * Debugging script
    
    * Debugging scriptgit
    
    * Debugging script
    
    * comment running fast integration tests temporarily
    
    * increase timeout
    
    * add verbosity to the undeploy command
    
    * rerun pipeline
    
    * Empty commit
    
    * rerun pipeline
    
    * increasing timeout
    
    * revert timeout change
    
    * add sleep after integration tests
    
    * remove timeout
    
    * remove fast integration tests
    
    * Empty commit

--- prow/pre-cli-e2e.sh
+++ prow/pre-cli-e2e.sh
@@ -1,10 +1,7 @@
 set -e
-apk add nodejs npm
 make resolve
 make build-linux
 cp ./bin/kyma-linux /usr/local/bin/kyma
 kyma provision k3d --ci
 kyma deploy --ci
-make -C "../kyma/tests/fast-integration" "ci"
-#TODO: Uncomment when https://github.com/kyma-project/cli/issues/1701 is done
-#kyma undeploy --ci --timeout=10m0s
+kyma undeploy --ci --timeout=10m0s
