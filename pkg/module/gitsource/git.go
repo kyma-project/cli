2299211a2e2d44bb1269672370010472b8bd2a52
Badr, Nesma <nesma.badr@sap.com>
2023-09-18 17:10:20 +0200

    Revert "revert changes"
    This reverts commit 150cb245fd2cafb95a8a08dbaeb9a6f15670f15f.

--- pkg/module/gitsource/git.go
+++ pkg/module/gitsource/git.go
@@ -14,17 +14,12 @@ import (
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/cpi"
 )
 
-func (g GitSource) FetchSource(ctx cpi.Context, path, repo, version, gitRemote string) (*ocm.Source, error) {
+func (g GitSource) FetchSource(ctx cpi.Context, path, repo, version string) (*ocm.Source, error) {
 	ref, commit, err := g.getGitInfo(path)
 	if err != nil {
 		return nil, err
 	}
 
-	if repo == "" {
-		if repo, err = g.determineRepositoryURL(gitRemote); err != nil {
-			return nil, err
-		}
-	}
 	sourceType := "git"
 	if !compatattr.Get(ctx) {
 		sourceType = github.CONSUMER_TYPE
@@ -52,8 +47,8 @@ func (g GitSource) FetchSource(ctx cpi.Context, path, repo, version, gitRemote s
 	}, nil
 }
 
-func (g GitSource) determineRepositoryURL(gitRemote string) (string, error) {
-	r, err := git.PlainOpen(".")
+func (g GitSource) DetermineRepositoryURL(gitRemote string, repoPath string) (string, error) {
+	r, err := git.PlainOpen(repoPath)
 	if err != nil {
 		return "", fmt.Errorf("could not open git repository: %w", err)
 	}
