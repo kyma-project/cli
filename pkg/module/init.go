b0f945c97b295c8892c5b16fe1ff21cb72326a22
Xin Ruan <xin.ruan@sap.com>
2023-09-21 15:31:56 +0200

    use ociblob instead of ocilocalblob for oci v2 (#1768)
    * remove compatible attribute for v2, so that v2 generate resources with ociblob instead of depricated ocilocalblob
    
    * add e2e test
    
    * add e2e test
    
    * add module config test
    
    * add module config test
    
    * fix github action
    
    * extend e2e test
    drop gotest.tools
    
    * fix lint
    
    * test repoURL
    
    * fix TEST_REPOSITORY_URL location
    
    * gofmt
    
    * align name

--- pkg/module/init.go
+++ pkg/module/init.go
@@ -4,8 +4,8 @@ import (
 	"github.com/open-component-model/ocm/pkg/contexts/oci"
 	"github.com/open-component-model/ocm/pkg/contexts/oci/repositories/ocireg"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm"
-	"github.com/open-component-model/ocm/pkg/contexts/ocm/compdesc"
-	"github.com/open-component-model/ocm/pkg/contexts/ocm/compdesc/versions/ocm.software/v3alpha1"
+	// nolint:revive
+	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/compdesc/versions"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/cpi"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/repositories/comparch"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/repositories/genericocireg"
@@ -13,7 +13,6 @@ import (
 
 // nolint:gochecknoinits
 func init() {
-	compdesc.RegisterScheme(&v3alpha1.DescriptorVersion{})
 	ocm.DefaultContext().RepositoryTypes().Register(ocireg.Type, genericocireg.NewRepositoryType(oci.DefaultContext()))
 	ocm.DefaultContext().RepositoryTypes().Register(
 		ocireg.LegacyType, genericocireg.NewRepositoryType(oci.DefaultContext()),
