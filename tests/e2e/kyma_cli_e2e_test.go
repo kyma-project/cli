720c54165ad8ae207b84be8a428e82c93ae46993
Badr, Nesma <nesma.badr@sap.com>
2023-09-28 16:34:20 +0200

    remove unneeded expect statement

--- tests/e2e/kyma_cli_e2e_test.go
+++ tests/e2e/kyma_cli_e2e_test.go
@@ -2,10 +2,8 @@ package e2e
 
 import (
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Kyma Deployment, Enabling and Disabling", Ordered, func() {
 	// TODO: apply the module template
-	Expect(1).To(Equal(0))
 })
