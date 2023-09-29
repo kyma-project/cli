838266b814f172749eaa5df663a9ca3101eccfc7
Badr, Nesma <nesma.badr@sap.com>
2023-09-29 12:48:53 +0200

    fix

--- tests/e2e/kyma_cli_e2e_test.go
+++ tests/e2e/kyma_cli_e2e_test.go
@@ -16,17 +16,23 @@ var _ = Describe("Kyma Deployment, Enabling and Disabling", Ordered, func() {
 	kcpSystemNamespace := "kcp-system"
 
 	It("Then should install Kyma and Lifecycle Manager successfully", func() {
-		When("Executing kyma alpha deploy command")
-		deployCmd := exec.Command("kyma", "alpha", "deploy")
-		deployOut, err := deployCmd.CombinedOutput()
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(deployOut).Should(ContainSubstring("Happy Kyma-ing!"))
+		When("Executing kyma alpha deploy command", func() {
+			deployCmd := exec.Command("kyma", "alpha", "deploy")
+			deployOut, err := deployCmd.CombinedOutput()
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(deployOut).Should(ContainSubstring("Happy Kyma-ing!"))
+		})
 
-		By("Then Kyma CR should be deployed")
+		By("Then Kyma CR should be deployed", func() {
 
-		By("Then Lifecycle Manager should be deployed")
-		lifecycleManager, err := client.AppsV1().Deployments(kcpSystemNamespace).
-			Get(ctx, "lifecycle-manager-controller-manager", v1.GetOptions{})
-		GinkgoWriter.Println(lifecycleManager.Status)
+		})
+
+		By("Then Lifecycle Manager should be deployed", func() {
+			lifecycleManager, err := client.AppsV1().Deployments(kcpSystemNamespace).
+				Get(ctx, "lifecycle-manager-controller-manager", v1.GetOptions{})
+			Expect(err).NotTo(HaveOccurred())
+			GinkgoWriter.Println(lifecycleManager.Status)
+		})
+
 	})
 })
