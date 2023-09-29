a88a7dc9b6c59a515b9c1f8a8126ea3e2bb1caff
Badr, Nesma <nesma.badr@sap.com>
2023-09-29 13:10:46 +0200

    fix

--- tests/e2e/kyma_cli_e2e_test.go
+++ tests/e2e/kyma_cli_e2e_test.go
@@ -16,23 +16,24 @@ var _ = Describe("Kyma Deployment, Enabling and Disabling", Ordered, func() {
 	kcpSystemNamespace := "kcp-system"
 
 	It("Then should install Kyma and Lifecycle Manager successfully", func() {
-		By("Executing kyma alpha deploy command", func() {
-			deployCmd := exec.Command("kyma", "alpha", "deploy")
-			deployOut, err := deployCmd.CombinedOutput()
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(deployOut).Should(ContainSubstring("Happy Kyma-ing!"))
-		})
+		By("Executing kyma alpha deploy command")
+		deployCmd := exec.Command("kyma", "alpha", "deploy")
+		deployOut, err := deployCmd.CombinedOutput()
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(deployOut).Should(ContainSubstring("Happy Kyma-ing!"))
 
-		By("Then Kyma CR should be deployed", func() {
+		//By("Then Kyma CR should be deployed")
+		//kymaResource := schema.GroupVersionResource{
+		//	Group:    "operator.kyma-project.io",
+		//	Version:  "v1beta2",
+		//	Resource: "kymas",
+		//}
 
-		})
-
-		By("Then Lifecycle Manager should be Ready", func() {
-			Eventually(IsDeploymentReady).
-				WithContext(ctx).
-				WithArguments(client, "lifecycle-manager-controller-manager", kcpSystemNamespace).
-				Should(BeTrue())
-		})
+		By("Then Lifecycle Manager should be Ready")
+		Eventually(IsDeploymentReady).
+			WithContext(ctx).
+			WithArguments(client, "lifecycle-manager-controller-manager", kcpSystemNamespace).
+			Should(BeTrue())
 
 	})
 })
