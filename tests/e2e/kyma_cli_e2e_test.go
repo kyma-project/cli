579e93255403ca047dd0650edfab04640b6f0326
Badr, Nesma <nesma.badr@sap.com>
2023-09-29 11:37:16 +0200

    start test implementation

--- tests/e2e/kyma_cli_e2e_test.go
+++ tests/e2e/kyma_cli_e2e_test.go
@@ -1,9 +1,17 @@
 package e2e
 
 import (
+	"os/exec"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Kyma Deployment, Enabling and Disabling", Ordered, func() {
-	// TODO: apply the module template
+	It("Should execute kyma alpha deploy command", func() {
+		deployCmd := exec.Command("kyma", "alpha", "deploy")
+		deployOut, err := deployCmd.CombinedOutput()
+		Expect(err).NotTo(HaveOccurred())
+		GinkgoWriter.Println(string(deployOut))
+	})
 })
