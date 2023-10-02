3ec3ad091d5677e8cba6782b3fc946a2d87da889
Badr, Nesma <nesma.badr@sap.com>
2023-10-02 10:07:53 +0200

    revert module creation test

--- tests/e2e/kyma_create_module_test.go
+++ tests/e2e/kyma_create_module_test.go
@@ -2,13 +2,10 @@ package e2e_test
 
 import (
 	"os"
+	"testing"
 
 	"github.com/kyma-project/cli/pkg/module"
-	. "github.com/kyma-project/cli/tests/e2e"
-	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/kyma-project/cli/tests/e2e"
 	"github.com/open-component-model/ocm/pkg/contexts/oci/repositories/ocireg"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/github"
@@ -19,81 +16,73 @@ import (
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/repositories/genericocireg"
 	ocmOCIReg "github.com/open-component-model/ocm/pkg/contexts/ocm/repositories/ocireg"
 	"gopkg.in/yaml.v3"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Describe("Module Creation", Ordered, func() {
+func Test_ModuleTemplate(t *testing.T) {
 	moduleTemplateVersion := os.Getenv("MODULE_TEMPLATE_VERSION")
 	ociRepoURL := os.Getenv("OCI_REPOSITORY_URL")
 	testRepoURL := os.Getenv("TEST_REPOSITORY_URL")
-	var descriptor *v1beta2.Descriptor
 
-	It("When ModuleTemplate is parsed", func() {
-		template, err := ReadModuleTemplate(os.Getenv("MODULE_TEMPLATE_PATH"))
-		Expect(err).To(Not(HaveOccurred()))
-		descriptor, err = template.GetDescriptor()
-		Expect(err).To(Not(HaveOccurred()))
-		Expect(descriptor.SchemaVersion()).To(Equal(v2.SchemaVersion))
-	})
+	template, err := e2e.ReadModuleTemplate(os.Getenv("MODULE_TEMPLATE_PATH"))
+	assert.Nil(t, err)
+	descriptor, err := template.GetDescriptor()
+	assert.Nil(t, err)
+	assert.Equal(t, descriptor.SchemaVersion(), v2.SchemaVersion)
 
-	It("Then descriptor.component.repositoryContexts should be correct", func() {
-		Expect(len(descriptor.RepositoryContexts)).To(Equal(1))
-		unstructuredRepo := descriptor.GetEffectiveRepositoryContext()
-		typedRepo, err := unstructuredRepo.Evaluate(cpi.DefaultContext().RepositoryTypes())
-		Expect(err).To(Not(HaveOccurred()))
-		concreteRepo, ok := typedRepo.(*genericocireg.RepositorySpec)
-		Expect(ok).To(BeTrue())
-		Expect(concreteRepo.ComponentNameMapping).To(Equal(ocmOCIReg.OCIRegistryURLPathMapping))
-		Expect(concreteRepo.GetType()).To(Equal(ocireg.Type))
-		Expect(concreteRepo.Name()).To(Equal(ociRepoURL))
-	})
+	// test descriptor.component.repositoryContexts
+	assert.Equal(t, len(descriptor.RepositoryContexts), 1)
+	unstructuredRepo := descriptor.GetEffectiveRepositoryContext()
+	typedRepo, err := unstructuredRepo.Evaluate(cpi.DefaultContext().RepositoryTypes())
+	assert.Nil(t, err)
+	concreteRepo, ok := typedRepo.(*genericocireg.RepositorySpec)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, concreteRepo.ComponentNameMapping, ocmOCIReg.OCIRegistryURLPathMapping)
+	assert.Equal(t, concreteRepo.GetType(), ocireg.Type)
+	assert.Equal(t, concreteRepo.Name(), ociRepoURL)
 
-	It("Then descriptor.component.resources should be correct", func() {
-		Expect(len(descriptor.Resources)).To(Equal(1))
-		resource := descriptor.Resources[0]
-		Expect(resource.Name).To(Equal(module.RawManifestLayerName))
-		Expect(resource.Relation).To(Equal(ocmMetaV1.LocalRelation))
-		Expect(resource.Type).To(Equal(module.TypeYaml))
-		Expect(resource.Version).To(Equal(moduleTemplateVersion))
+	// test descriptor.component.resources[0]
+	assert.Equal(t, len(descriptor.Resources), 1)
+	resource := descriptor.Resources[0]
+	assert.Equal(t, resource.Name, module.RawManifestLayerName)
+	assert.Equal(t, resource.Relation, ocmMetaV1.LocalRelation)
+	assert.Equal(t, resource.Type, module.TypeYaml)
+	assert.Equal(t, resource.Version, moduleTemplateVersion)
 
-		resourceAccessSpec, err := ocm.DefaultContext().AccessSpecForSpec(resource.Access)
-		Expect(err).To(Not(HaveOccurred()))
-		localblobAccessSpec, ok := resourceAccessSpec.(*localblob.AccessSpec)
-		Expect(ok).To(BeTrue())
-		Expect(localblobAccessSpec.GetType()).To(Equal(localblob.Type))
-		Expect(localblobAccessSpec.LocalReference).To(HavePrefix("sha256:"))
-	})
+	// test descriptor.component.resources[0].access
+	resourceAccessSpec, err := ocm.DefaultContext().AccessSpecForSpec(resource.Access)
+	assert.Nil(t, err)
+	localblobAccessSpec, ok := resourceAccessSpec.(*localblob.AccessSpec)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, localblobAccessSpec.GetType(), localblob.Type)
+	assert.Contains(t, localblobAccessSpec.LocalReference, "sha256:")
 
-	It("Then descriptor.component.sources should be correct", func() {
-		Expect(len(descriptor.Sources)).To(Equal(1))
-		source := descriptor.Sources[0]
-		sourceAccessSpec, err := ocm.DefaultContext().AccessSpecForSpec(source.Access)
-		Expect(err).To(Not(HaveOccurred()))
-		githubAccessSpec, ok := sourceAccessSpec.(*github.AccessSpec)
-		Expect(ok).To(BeTrue())
-		Expect(githubAccessSpec.Type).To(Equal(github.Type))
-		Expect(testRepoURL).To(ContainSubstring(githubAccessSpec.RepoURL))
-	})
+	// test descriptor.component.sources
+	assert.Equal(t, len(descriptor.Sources), 1)
+	source := descriptor.Sources[0]
+	sourceAccessSpec, err := ocm.DefaultContext().AccessSpecForSpec(source.Access)
+	assert.Nil(t, err)
+	githubAccessSpec, ok := sourceAccessSpec.(*github.AccessSpec)
+	assert.Equal(t, githubAccessSpec.Type, github.Type)
+	assert.Contains(t, testRepoURL, githubAccessSpec.RepoURL)
 
-	It("Then security scan labels should be correct", func() {
-		secScanLabels := descriptor.Sources[0].Labels
-		var devBranch string
-		err := yaml.Unmarshal(secScanLabels[1].Value, &devBranch)
-		Expect(err).To(Not(HaveOccurred()))
-		Expect(devBranch).To(Equal("main"))
+	// test security scan labels
+	secScanLabels := descriptor.Sources[0].Labels
 
-		var rcTag string
-		err = yaml.Unmarshal(secScanLabels[2].Value, &rcTag)
-		Expect(err).To(Not(HaveOccurred()))
-		Expect(rcTag).To(Equal("0.5.0"))
+	var devBranch string
+	yaml.Unmarshal(secScanLabels[1].Value, &devBranch)
+	assert.Equal(t, "main", devBranch)
 
-		var language string
-		err = yaml.Unmarshal(secScanLabels[3].Value, &language)
-		Expect(err).To(Not(HaveOccurred()))
-		Expect(language).To(Equal("golang-mod"))
+	var rcTag string
+	yaml.Unmarshal(secScanLabels[2].Value, &rcTag)
+	assert.Equal(t, "0.5.0", rcTag)
 
-		var exclude string
-		err = yaml.Unmarshal(secScanLabels[4].Value, &exclude)
-		Expect(err).To(Not(HaveOccurred()))
-		Expect(exclude).To(Equal("**/test/**,**/*_test.go"))
-	})
-})
+	var language string
+	yaml.Unmarshal(secScanLabels[3].Value, &language)
+	assert.Equal(t, "golang-mod", language)
+
+	var exclude string
+	yaml.Unmarshal(secScanLabels[4].Value, &exclude)
+	assert.Equal(t, "**/test/**,**/*_test.go", exclude)
+}
