4f356a0ee5c8b706c963c31daf0615e98b32d2e6
jeremyharisch <jeremy@harisch.de>
2024-01-16 14:27:55 +0100

    Set 1.27.9 as default K8s Version

--- cmd/kyma/provision/cmd.go
+++ cmd/kyma/provision/cmd.go
@@ -4,9 +4,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const DefaultK8sShortVersion = "1.26"                       //default Kubernetes version for provisioning clusters on hyperscalers
-const DefaultK8sFullVersion = DefaultK8sShortVersion + ".6" //default Kubernetes version with the "patch" component (mainly for K3d/K3s)
-const DefaultGardenLinuxVersion = "934.9.0"                 //default Garden Linux version
+const DefaultK8sShortVersion = "1.27"                       // default Kubernetes version for provisioning clusters on hyperscalers
+const DefaultK8sFullVersion = DefaultK8sShortVersion + ".9" // default Kubernetes version with the "patch" component (mainly for K3d/K3s)
+const DefaultGardenLinuxVersion = "934.9.0"                 // default Garden Linux version
 
 // NewCmd creates a new provision command
 func NewCmd() *cobra.Command {
