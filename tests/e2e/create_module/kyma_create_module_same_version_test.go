ff0381703bec6e0694be202766646be691fa9b66
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2024-02-02 12:41:42 +0100

    cleanup

--- tests/e2e/create_module/kyma_create_module_same_version_test.go
+++ tests/e2e/create_module/kyma_create_module_same_version_test.go
@@ -1,7 +1,6 @@
 package create_module_test
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
@@ -16,48 +15,27 @@ const (
 )
 
 func Test_SameVersion_ModuleCreation(t *testing.T) {
-	path := "../" + "../../../template-operator"
-	configFilePath := "../" + "../../../template-operator/module-config.yaml"
-	secScannerConfigFile := "../" + "../../../template-operator/sec-scanners-config.yaml"
-	changedSecScannerConfigFile := "../" + "../../../template-operator/sec-scanners-config-changed.yaml"
+	path := "../../../template-operator"
+	configFilePath := "../../../template-operator/module-config.yaml"
+	secScannerConfigFile := "../../../template-operator/sec-scanners-config.yaml"
+	changedSecScannerConfigFile := "../../../template-operator/sec-scanners-config-changed.yaml"
 	version := os.Getenv(moduleTemplateVersionEnvVar)
 	registry := os.Getenv(ociRepositoryEnvVar)
 
 	t.Run("Create same version module with module-archive-version-overwrite flag", func(t *testing.T) {
 		err := e2e.CreateModuleCommand(true, path, registry, configFilePath, version, secScannerConfigFile)
-		moveDir(".bak1")
 		assert.Nil(t, err)
 	})
 
 	t.Run("Create same version module and same content without module-archive-version-overwrite flag",
 		func(t *testing.T) {
 			err := e2e.CreateModuleCommand(false, path, registry, configFilePath, version, secScannerConfigFile)
-			moveDir(".bak2")
 			assert.Nil(t, err)
 		})
 
 	t.Run("Create same version module, but different content without module-archive-version-overwrite flag",
 		func(t *testing.T) {
 			err := e2e.CreateModuleCommand(false, path, registry, configFilePath, version, changedSecScannerConfigFile)
-			moveDir(".bak3")
 			assert.Equal(t, e2e.ErrCreateModuleFailedWithSameVersion, err)
 		})
-}
-
-func moveDir(suffix string) {
-	oldName := "mod"
-	fi, err := os.Stat(oldName)
-	if err != nil {
-		fmt.Printf("error during os.Stat(\"mod\"): %s\n", err.Error())
-		return
-	}
-	if fi.IsDir() {
-		newName := oldName + suffix
-		err = os.Rename(oldName, newName)
-		if err != nil {
-			fmt.Printf("error during os.Rename(oldName,newName): %s\n", err.Error())
-		}
-	} else {
-		fmt.Printf("%s is not a dir!\n", oldName)
-	}
 }
