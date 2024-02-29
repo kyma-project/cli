524aa96e2de1fc528334253365166f3e69def2a9
Piotr Halama <piotr.halama@sap.com>
2024-02-29 09:21:10 +0100

    kyma provision (#1964)

--- internal/cmd/provision/provision.go
+++ internal/cmd/provision/provision.go
@@ -1,7 +1,6 @@
 package provision
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/kyma-project/cli.v3/internal/btp"
@@ -10,13 +9,20 @@ import (
 
 type provisionConfig struct {
 	credentialsPath string
+	plan            string
+	environmentName string
+	clusterName     string
+	region          string
 }
 
 func NewProvisionCMD() *cobra.Command {
 	config := provisionConfig{}
 
 	cmd := &cobra.Command{
-		Use: "provision",
+		Use:   "provision",
+		Short: "Provisions a Kyma cluster on the BTP.",
+		Long: `Use this command to provision a Kyma environment on the SAP BTP platform.
+`,
 		RunE: func(_ *cobra.Command, _ []string) error {
 			return runProvision(&config)
 		},
@@ -24,6 +30,12 @@ func NewProvisionCMD() *cobra.Command {
 
 	cmd.PersistentFlags()
 	cmd.Flags().StringVar(&config.credentialsPath, "credentials-path", "", "Path to the CIS credentials file.")
+
+	cmd.Flags().StringVar(&config.plan, "plan", "trial", "Name of the Kyma environment plan, e.g trial, azure, aws, gcp.")
+	cmd.Flags().StringVar(&config.environmentName, "environment-name", "kyma", "Name of the environment in the BTP.")
+	cmd.Flags().StringVar(&config.clusterName, "cluster-name", "kyma", "Name of the Kyma cluster.")
+	cmd.Flags().StringVar(&config.region, "region", "", "Name of the region of the Kyma cluster.")
+
 	_ = cmd.MarkFlagRequired("credentials-path")
 
 	return cmd
@@ -40,13 +52,24 @@ func runProvision(config *provisionConfig) error {
 		return fmt.Errorf("failed to get access token: %s", err.Error())
 	}
 
-	// TODO: remove in next interation
-	data, err := json.Marshal(token)
+	localCISClient := btp.NewLocalClient(credentials, token)
+
+	ProvisionEnvironment := &btp.ProvisionEnvironment{
+		EnvironmentType: "kyma",
+		PlanName:        config.plan,
+		Name:            config.environmentName,
+		User:            "kyma-cli",
+		Parameters: btp.KymaParameters{
+			Name:   config.clusterName,
+			Region: config.region,
+		},
+	}
+	response, err := localCISClient.Provision(ProvisionEnvironment)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to provision kyma runtime: %s", err.Error())
 	}
 
-	fmt.Printf("%s\n", data)
+	fmt.Printf("Kyma environment provisioning, environment name: '%s', id: '%s'\n", response.Name, response.ID)
 
 	return nil
 }
