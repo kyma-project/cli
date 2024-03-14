b33b48dac73858658baaa1869887ac0def725bec
Filip Strozik <filip.strozik@outlook.com>
2024-03-14 14:00:25 +0100

    Implement the `image-import` command (#1991)
    * import image command
    
    * reviewv1
    
    * comment portforward package
    
    ---------
    
    Co-authored-by: Andrzej Pankowski <andrzej.pankowski@sap.com>

---
+++ internal/cmd/imageimport/imageimport.go
@@ -0,0 +1,107 @@
+package imageimport
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/kyma-project/cli.v3/internal/kube"
+	"github.com/kyma-project/cli.v3/internal/registry"
+	"github.com/spf13/cobra"
+)
+
+type provisionConfig struct {
+	ctx        context.Context
+	kubeClient kube.Client
+
+	image      string
+	kubeconfig string
+}
+
+func NewImportCMD() *cobra.Command {
+	config := provisionConfig{}
+
+	cmd := &cobra.Command{
+		Use:   "image-import",
+		Short: "Import image to in-cluster registry.",
+		Long:  `Import image from daemon to in-cluster registry.`,
+		Args:  cobra.ExactArgs(1),
+
+		PreRunE: func(_ *cobra.Command, args []string) error {
+			if err := config.complete(args); err != nil {
+				return err
+			}
+			return config.validate()
+		},
+		RunE: func(_ *cobra.Command, args []string) error {
+			return runImageImport(&config)
+		},
+	}
+
+	cmd.Flags().StringVar(&config.kubeconfig, "kubeconfig", "", "Path to the kubeconfig file.")
+
+	return cmd
+}
+
+func (pc *provisionConfig) validate() error {
+	imageElems := strings.Split(pc.image, ":")
+	if len(imageElems) != 2 {
+		return fmt.Errorf("image '%s' not in expected format 'image:tag'", pc.image)
+	}
+
+	return nil
+}
+
+func (pc *provisionConfig) complete(args []string) error {
+	pc.image = args[0]
+
+	// TODO: think about itmeout and moving context to persistent `kyma` command configuration
+	pc.ctx = context.Background()
+
+	var err error
+	pc.kubeClient, err = kube.NewClient(pc.kubeconfig)
+
+	return err
+}
+
+func runImageImport(config *provisionConfig) error {
+	// TODO: Add "serverless is not installed" error message
+	registryConfig, err := registry.GetConfig(config.ctx, config.kubeClient.Static())
+	if err != nil {
+		return fmt.Errorf("failed to load in-cluster registry configuration: %s", err.Error())
+	}
+
+	if !registryConfig.IsInternal {
+		return errors.New("this command does not work for external docker registry")
+	}
+
+	workloadMeta, err := registry.GetWorkloadMeta(config.ctx, config.kubeClient.Static(), registryConfig)
+	if err != nil {
+		return fmt.Errorf("failed to load in-cluster registry pod content: %s", err.Error())
+	}
+
+	fmt.Println("Importing", config.image)
+
+	pushedImage, err := registry.ImportImage(
+		config.ctx,
+		config.image,
+		registry.ImportOptions{
+			ClusterAPIRestConfig: config.kubeClient.RestConfig(),
+			RegistryAuth:         registry.NewBasicAuth(registryConfig.Username, registryConfig.Password),
+			RegistryPullHost:     registryConfig.PullRegAddr,
+			RegistryPodName:      workloadMeta.Name,
+			RegistryPodNamespace: workloadMeta.Namespace,
+			RegistryPodPort:      workloadMeta.Port,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("faile to import image to in-cluster registry: %s", err.Error())
+	}
+
+	fmt.Println("\nSuccessfully imported image")
+	fmt.Printf("Use it as '%s' and attach the 'serverless-function' ServiceAccount to the pod.\n", pushedImage)
+	fmt.Printf("\nExample usage:\nkubectl run my-pod --image=%s --overrides='{ \"spec\": { \"serviceAccount\": \"serverless-function\" }  }'\n", pushedImage)
+
+	return nil
+}
---
+++ internal/registry/portforward/transport.go
@@ -0,0 +1,120 @@
+package portforward
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/httpstream"
+)
+
+var (
+	NewRetryTransport = transport.NewRetry
+)
+
+// portforwardTransport forwards requests sent by the client to the port-forwarded pod from the cluster
+type portforwardTransport struct {
+	remoteConn httpstream.Connection
+	remotePort string
+}
+
+// NewPortforwardTransport creates http.RoundTripper implementation for given portforward connection and port
+func NewPortforwardTransport(remoteConn httpstream.Connection, remotePort string) http.RoundTripper {
+	return &portforwardTransport{
+		remoteConn: remoteConn,
+		remotePort: remotePort,
+	}
+}
+
+func (pft *portforwardTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	return pft.dialRemote(req)
+}
+
+
+// dialRemote is forwarding any incoming request to the stream
+// the logic is mostly based on the k8s.io/client-go/tools/portforward package
+// https://github.com/kubernetes/client-go/blob/271d034e86108101a804541843d50abe3fea06ae/tools/portforward/portforward.go#L335
+func (pft *portforwardTransport) dialRemote(req *http.Request) (*http.Response, error) {
+	forwardID := rand.Int()
+
+	// create error stream
+	errorStream, err := pft.createErrorStream(forwardID)
+	if err != nil {
+		return nil, fmt.Errorf("error creating error stream for port %s: %v", pft.remotePort, err)
+	}
+	// close stream to inform remote server that we are not going to send any data,
+	// and that we are ready to receive the errors
+	errorStream.Close()
+	defer pft.remoteConn.RemoveStreams(errorStream)
+
+	errorChan := make(chan error)
+	go handleErrorStream(errorStream, errorChan)
+
+	// create data stream
+	dataStream, err := pft.createDataStream(forwardID)
+	if err != nil {
+		return nil, fmt.Errorf("error creating data stream for port %s: %v", pft.remotePort, err)
+	}
+	defer dataStream.Close()
+	defer pft.remoteConn.RemoveStreams(dataStream)
+
+	resp, err := dialToStream(dataStream, req)
+	if err != nil {
+		return nil, err
+	}
+
+	// always expect something on errorChan (it may be nil)
+	err = <-errorChan
+
+	return resp, err
+}
+
+func (pft *portforwardTransport) createErrorStream(forwardID int) (httpstream.Stream, error) {
+	return createStream(pft.remoteConn, pft.remotePort, v1.StreamTypeError, forwardID)
+}
+
+func (pft *portforwardTransport) createDataStream(forwardID int) (httpstream.Stream, error) {
+	return createStream(pft.remoteConn, pft.remotePort, v1.StreamTypeData, forwardID)
+}
+
+func createStream(remoteConn httpstream.Connection, remotePort, streamType string, forwardID int) (httpstream.Stream, error) {
+	headers := http.Header{}
+	headers.Set(v1.StreamType, streamType)
+	headers.Set(v1.PortHeader, remotePort)
+	headers.Set(v1.PortForwardRequestIDHeader, strconv.Itoa(forwardID))
+
+	return remoteConn.CreateStream(headers)
+}
+
+func dialToStream(dataStream httpstream.Stream, req *http.Request) (*http.Response, error) {
+	if err := req.Write(dataStream); err != nil && !strings.Contains(err.Error(), "use of closed network connection") {
+		return nil, fmt.Errorf("error writing to remote stream: %v", err)
+	}
+
+	// close stream to inform remote server that there is nothing more to receive
+	dataStream.Close()
+
+	resp, err := http.ReadResponse(bufio.NewReader(dataStream), req)
+	if err != nil && !strings.Contains(err.Error(), "use of closed network connection") {
+		return nil, fmt.Errorf("error reading from remote stream: %v", err)
+	}
+
+	return resp, nil
+}
+
+func handleErrorStream(errorStream httpstream.Stream, errorChan chan error) {
+	message, err := io.ReadAll(errorStream)
+	switch {
+	case err != nil:
+		errorChan <- fmt.Errorf("error reading from error stream: %v", err)
+	case len(message) > 0:
+		errorChan <- fmt.Errorf("an error occurred while forwarding: %v", string(message))
+	}
+	close(errorChan)
+}
