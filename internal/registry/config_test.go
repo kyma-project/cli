fd4a417a0d366751322f24166f227d091494eef3
Filip Strozik <filip.strozik@outlook.com>
2024-05-13 14:02:45 +0200

    add missing test

--- internal/registry/config_test.go
+++ internal/registry/config_test.go
@@ -4,35 +4,72 @@ import (
 	"context"
 	"testing"
 
+	kube_fake "github.com/kyma-project/cli.v3/internal/kube/fake"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/client-go/kubernetes/fake"
+	dynamic_fake "k8s.io/client-go/dynamic/fake"
+	k8s_fake "k8s.io/client-go/kubernetes/fake"
 )
+
+func TestGetConfig(t *testing.T) {
+	t.Run("Should return the RegistryConfig", func(t *testing.T) {
+		// given
+		testRegistrySvc := fixTestRegistrySvc()
+		testRegistryPod := fixTestRegistryPod()
+		testRegistrySecret := fixTestRegistrySecret()
+		testDockerRegistry := fixTestDockerRegistry()
+
+		client := k8s_fake.NewSimpleClientset(testRegistrySvc, testRegistryPod, testRegistrySecret)
+
+		scheme := runtime.NewScheme()
+		scheme.AddKnownTypes(DockerRegistryGVR.GroupVersion(), testDockerRegistry)
+		dynamic := dynamic_fake.NewSimpleDynamicClient(scheme, testDockerRegistry)
+
+		expectedRegistryConfig := &RegistryConfig{
+			SecretName: testRegistrySecret.GetName(),
+			SecretData: &SecretData{
+				DockerConfigJson: string(testRegistrySecret.Data[".dockerconfigjson"]),
+				Username:         string(testRegistrySecret.Data["username"]),
+				Password:         string(testRegistrySecret.Data["password"]),
+				PullRegAddr:      string(testRegistrySecret.Data["pullRegAddr"]),
+				PushRegAddr:      string(testRegistrySecret.Data["pushRegAddr"]),
+			},
+			PodMeta: &RegistryPodMeta{
+				Name:      "serverless-docker-registry-7d4d7b7b4f-7z5zv",
+				Namespace: "test-namespace",
+				Port:      "5000",
+			},
+		}
+
+		kubeClient := &kube_fake.FakeKubeClient{
+			TestKubernetesInterface: client,
+			TestDynamicInterface:    dynamic,
+		}
+
+		// when
+		config, err := GetConfig(context.Background(), kubeClient)
+
+		// then
+		require.NoError(t, err)
+		require.Equal(t, expectedRegistryConfig, config)
+	})
+}
 
 func Test_getRegistrySecretConfig(t *testing.T) {
 	t.Run("Should return the RegistryConfig", func(t *testing.T) {
 		// given
-		client := fake.NewSimpleClientset(&corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test-secret",
-				Namespace: "test-namespace",
-			},
-			Data: map[string][]byte{
-				".dockerconfigjson": []byte(`{"auths": {}}`),
-				"username":          []byte("testUsername"),
-				"password":          []byte("testPassword"),
-				"pullRegAddr":       []byte("pullRegAddr"),
-				"pushRegAddr":       []byte("pushRegAddr"),
-			},
-		})
+		testRegistrySecret := fixTestRegistrySecret()
+		client := k8s_fake.NewSimpleClientset(testRegistrySecret)
 		expectedRegistryConfig := &SecretData{
-			DockerConfigJson: `{"auths": {}}`,
-			Username:         "testUsername",
-			Password:         "testPassword",
-			PullRegAddr:      "pullRegAddr",
-			PushRegAddr:      "pushRegAddr",
+			DockerConfigJson: string(testRegistrySecret.Data[".dockerconfigjson"]),
+			Username:         string(testRegistrySecret.Data["username"]),
+			Password:         string(testRegistrySecret.Data["password"]),
+			PullRegAddr:      string(testRegistrySecret.Data["pullRegAddr"]),
+			PushRegAddr:      string(testRegistrySecret.Data["pushRegAddr"]),
 		}
 
 		// when
@@ -45,7 +82,7 @@ func Test_getRegistrySecretConfig(t *testing.T) {
 
 	t.Run("Should return an error when the secret does not exist", func(t *testing.T) {
 		// given
-		client := fake.NewSimpleClientset()
+		client := k8s_fake.NewSimpleClientset()
 		expectedErrorMsg := "secrets \"test-secret\" not found"
 
 		// when
@@ -61,44 +98,13 @@ func Test_getRegistrySecretConfig(t *testing.T) {
 func Test_getWorkloadMeta(t *testing.T) {
 	t.Run("Should return the RegistryPodMeta", func(t *testing.T) {
 		// given
-		client := fake.NewSimpleClientset(&corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "serverless-docker-registry",
-				Namespace: "kyma-system",
-			},
-			Spec: corev1.ServiceSpec{
-				Selector: map[string]string{
-					"app": "serverless-docker-registry",
-				},
-				Ports: []corev1.ServicePort{
-					{
-						TargetPort: intstr.FromString("5000"),
-					},
-				},
-			},
-		}, &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "serverless-docker-registry-7d4d7b7b4f-7z5zv",
-				Namespace: "kyma-system",
-				Labels: map[string]string{
-					"app": "serverless-docker-registry",
-				},
-			},
-			Status: corev1.PodStatus{
-				Conditions: []corev1.PodCondition{
-					{
-						Type:   corev1.ContainersReady,
-						Status: corev1.ConditionTrue,
-					},
-				},
-			},
-		})
+		client := k8s_fake.NewSimpleClientset(fixTestRegistrySvc(), fixTestRegistryPod())
 		config := &SecretData{
-			PushRegAddr: "serverless-docker-registry.kyma-system.svc.cluster.local:5000",
+			PushRegAddr: "serverless-docker-registry.test-namespace.svc.cluster.local:5000",
 		}
 		expectedRegistryPodMeta := &RegistryPodMeta{
 			Name:      "serverless-docker-registry-7d4d7b7b4f-7z5zv",
-			Namespace: "kyma-system",
+			Namespace: "test-namespace",
 			Port:      "5000",
 		}
 
@@ -111,24 +117,9 @@ func Test_getWorkloadMeta(t *testing.T) {
 	})
 	t.Run("Should return an error when no pods exist", func(t *testing.T) {
 		// given
-		client := fake.NewSimpleClientset(&corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "serverless-docker-registry",
-				Namespace: "kyma-system",
-			},
-			Spec: corev1.ServiceSpec{
-				Selector: map[string]string{
-					"app": "serverless-docker-registry",
-				},
-				Ports: []corev1.ServicePort{
-					{
-						TargetPort: intstr.FromString("5000"),
-					},
-				},
-			},
-		})
+		client := k8s_fake.NewSimpleClientset(fixTestRegistrySvc())
 		config := &SecretData{
-			PushRegAddr: "serverless-docker-registry.kyma-system.svc.cluster.local:5000",
+			PushRegAddr: "serverless-docker-registry.test-namespace.svc.cluster.local:5000",
 		}
 
 		// when
@@ -141,6 +132,74 @@ func Test_getWorkloadMeta(t *testing.T) {
 	})
 }
 
-func TestGetConfig(t *testing.T) {
-	// TODO: Add test cases in next PR.
+func fixTestRegistrySvc() *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "serverless-docker-registry",
+			Namespace: "test-namespace",
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"app": "serverless-docker-registry",
+			},
+			Ports: []corev1.ServicePort{
+				{
+					TargetPort: intstr.FromString("5000"),
+				},
+			},
+		},
+	}
+}
+
+func fixTestRegistryPod() *corev1.Pod {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "serverless-docker-registry-7d4d7b7b4f-7z5zv",
+			Namespace: "test-namespace",
+			Labels: map[string]string{
+				"app": "serverless-docker-registry",
+			},
+		},
+		Status: corev1.PodStatus{
+			Conditions: []corev1.PodCondition{
+				{
+					Type:   corev1.ContainersReady,
+					Status: corev1.ConditionTrue,
+				},
+			},
+		},
+	}
+}
+
+func fixTestRegistrySecret() *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-secret",
+			Namespace: "test-namespace",
+		},
+		Data: map[string][]byte{
+			".dockerconfigjson": []byte(`{"auths": {}}`),
+			"username":          []byte("testUsername"),
+			"password":          []byte("testPassword"),
+			"pullRegAddr":       []byte("localhost:32137"),
+			"pushRegAddr":       []byte("serverless-docker-registry.test-namespace.svc.cluster.local:5000"),
+		},
+	}
+}
+
+func fixTestDockerRegistry() *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "operator.kyma-project.io/v1alpha1",
+			"kind":       "DockerRegistry",
+			"metadata": map[string]interface{}{
+				"name":      "test-docker-registry",
+				"namespace": "test-namespace",
+			},
+			"status": map[string]interface{}{
+				"secretName": "test-secret",
+				"state":      "Ready",
+			},
+		},
+	}
 }
