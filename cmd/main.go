47cc8bfa9e6a60254ad7a17360f33f0efa1a7cb0
Filip Strozik <filip.strozik@outlook.com>
2024-05-20 08:22:25 +0200

    chore: Deprecate kyma-cli v2 (#2095)
    * draft
    
    * update warning
    
    * Apply suggestions from code review
    
    Co-authored-by: Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
    
    * Update README.md
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    ---------
    
    Co-authored-by: Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kyma-project/cli/cmd/kyma"
@@ -8,6 +9,8 @@ import (
 )
 
 func main() {
+	fmt.Print("WARNING: All commands within v2 are deprecated. We are designing v3 of kyma CLI  with a new set of commands that will be first released  within alpha command group.\n\n")
+
 	command := kyma.NewCmd(cli.NewOptions())
 
 	err := command.Execute()
