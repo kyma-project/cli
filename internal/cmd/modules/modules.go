3a3af6c471b504f72f6cca7cd184b2b68dedd8d6
Cortey <andrzej.pankowski@sap.com>
2024-06-05 10:18:29 +0200

    managed_flag

--- internal/cmd/modules/modules.go
+++ internal/cmd/modules/modules.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"github.com/kyma-project/cli.v3/internal/cmdcommon"
 	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"net/http"
+	"strings"
 
 	"github.com/kyma-project/cli.v3/internal/clierror"
 	"github.com/spf13/cobra"
@@ -21,8 +25,7 @@ type modulesConfig struct {
 }
 
 func NewModulesCMD(kymaConfig *cmdcommon.KymaConfig) *cobra.Command {
-
-	config := modulesConfig{
+	cfg := modulesConfig{
 		KymaConfig:       kymaConfig,
 		KubeClientConfig: cmdcommon.KubeClientConfig{},
 	}
@@ -32,16 +35,16 @@ func NewModulesCMD(kymaConfig *cmdcommon.KymaConfig) *cobra.Command {
 		Short: "List modules.",
 		Long:  `List either installed, managed or available Kyma modules.`,
 		PreRun: func(_ *cobra.Command, args []string) {
-			clierror.Check(config.KubeClientConfig.Complete())
+			clierror.Check(cfg.KubeClientConfig.Complete())
 		},
 		Run: func(_ *cobra.Command, _ []string) {
-			clierror.Check(runModules(&config))
+			clierror.Check(runModules(&cfg))
 		},
 	}
 
-	cmd.Flags().BoolVar(&config.catalog, "catalog", false, "List of al available Kyma modules.")
-	cmd.Flags().BoolVar(&config.managed, "managed", false, "List of all Kyma modules managed by central control-plane.")
-	cmd.Flags().BoolVar(&config.installed, "installed", false, "List of all currently installed Kyma modules.")
+	cmd.Flags().BoolVar(&cfg.catalog, "catalog", false, "List of al available Kyma modules.")
+	cmd.Flags().BoolVar(&cfg.managed, "managed", false, "List of all Kyma modules managed by central control-plane.")
+	cmd.Flags().BoolVar(&cfg.installed, "installed", false, "List of all currently installed Kyma modules.")
 
 	cmd.MarkFlagsOneRequired("catalog", "managed", "installed")
 	cmd.MarkFlagsMutuallyExclusive("catalog", "managed")
@@ -51,9 +54,9 @@ func NewModulesCMD(kymaConfig *cmdcommon.KymaConfig) *cobra.Command {
 	return cmd
 }
 
-func runModules(config *modulesConfig) clierror.Error {
+func runModules(cfg *modulesConfig) clierror.Error {
 	var err error
-	if config.catalog {
+	if cfg.catalog {
 		modules, err := listAllModules()
 		if err != nil {
 			return clierror.WrapE(err, clierror.New("failed to list all Kyma modules"))
@@ -64,15 +67,16 @@ func runModules(config *modulesConfig) clierror.Error {
 		}
 		return nil
 	}
-
-	if config.managed {
-		_, err := listManagedModules(config)
-		clierror.WrapE(err, clierror.New("not implemented yet, please use the catalog flag"))
+	if cfg.managed {
+		_, err := listManagedModules(cfg)
+		if err != nil {
+			return clierror.WrapE(err, clierror.New("failed to list managed Kyma modules"))
+		}
 		return nil
 	}
 
-	if config.installed {
-		clierror.Wrap(err, clierror.New("not implemented yet, please use the catalog flag"))
+	if cfg.installed {
+		clierror.Wrap(err, clierror.New("not implemented yet, please use the catalog or managed flag"))
 		return nil
 	}
 	//TODO: installed to implement
@@ -83,7 +87,7 @@ func runModules(config *modulesConfig) clierror.Error {
 func listAllModules() ([]string, clierror.Error) {
 	resp, err := http.Get("https://raw.githubusercontent.com/kyma-project/community-modules/main/model.json")
 	if err != nil {
-		return nil, clierror.Wrap(err, clierror.New("while getting modules list"))
+		return nil, clierror.Wrap(err, clierror.New("while getting modules list from github"))
 	}
 	defer resp.Body.Close()
 
@@ -107,7 +111,53 @@ func listAllModules() ([]string, clierror.Error) {
 	return out, nil
 }
 
-func listManagedModules(config *modulesConfig) ([]string, clierror.Error) {
+func listManagedModules(cfg *modulesConfig) ([]string, clierror.Error) {
+	GVRKyma := schema.GroupVersionResource{
+		Group:    "operator.kyma-project.io",
+		Version:  "v1beta2",
+		Resource: "kymas",
+	}
 
-	return nil, clierror.New("")
+	unstruct, err := cfg.KubeClient.Dynamic().Resource(GVRKyma).Namespace("kyma-system").Get(cfg.Ctx, "default", metav1.GetOptions{})
+	if err != nil {
+		return nil, clierror.Wrap(err, clierror.New("while getting Kyma CR"))
+	}
+
+	moduleNames, err := getModuleNames(unstruct)
+	if err != nil {
+		return nil, clierror.Wrap(err, clierror.New("while getting module names from CR"))
+	}
+
+	return moduleNames, nil
+}
+
+func getModuleNames(unstruct *unstructured.Unstructured) ([]string, error) {
+	var moduleNames []string
+	managedFields := unstruct.GetManagedFields()
+	for _, field := range managedFields {
+		var data map[string]interface{}
+		err := json.Unmarshal(field.FieldsV1.Raw, &data)
+		if err != nil {
+			return nil, err
+		}
+
+		spec, ok := data["f:spec"].(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		modules, ok := spec["f:modules"].(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		for key := range modules {
+			if strings.Contains(key, "name") {
+				name := strings.TrimPrefix(key, "k:{\"name\":\"")
+				name = strings.Trim(name, "\"}")
+				moduleNames = append(moduleNames, name)
+			}
+		}
+	}
+	return moduleNames, nil
 }
