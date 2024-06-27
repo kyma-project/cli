2ee106b37e22a2eb7b9d256e4e89a13fae0370e2
Piotr Halama <piotr.halama@sap.com>
2024-06-27 14:07:54 +0200

    fix: Change deprecation notice output to stderr (#2156)

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Print("WARNING: All commands within v2 are deprecated. We are designing v3 of kyma CLI  with a new set of commands that will be first released  within alpha command group.\n\n")
+	fmt.Fprint(os.Stderr, "WARNING: All commands within v2 are deprecated. We are designing v3 of kyma CLI with a new set of commands that will be first released within alpha command group.\n\n")
 
 	command := kyma.NewCmd(cli.NewOptions())
 
