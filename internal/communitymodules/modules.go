67be6a593db75de2e09c33c0725625e279293fb8
Andrzej Pankowski <andrzej.pankowski@sap.com>
2024-07-04 11:13:55 +0200

    Cover the case when one of the repository fields is empty in Modules command (#2174)

--- internal/communitymodules/modules.go
+++ internal/communitymodules/modules.go
@@ -46,13 +46,24 @@ func modulesCatalog(url string) (moduleMap, clierror.Error) {
 		latestVersion := getLatestVersion(rec.Versions)
 		catalog[rec.Name] = row{
 			Name:          rec.Name,
-			Repository:    latestVersion.Repository,
+			Repository:    chooseRepository(rec, latestVersion),
 			LatestVersion: latestVersion.Version,
 		}
 	}
 	return catalog, nil
 }
 
+// chooseRepository returns the repository of the module for specific version if it is available, otherwise it returns the repository of the module.
+// Sometimes one of those values don't exist so this function makes sure that we provide the user with the most information possible.
+func chooseRepository(module Module, version Version) string {
+	if version.Repository != "" {
+		return version.Repository
+	}
+	if module.Repository != "" {
+		return module.Repository
+	}
+	return "Unknown"
+}
 func getLatestVersion(versions []Version) Version {
 	return slices.MaxFunc(versions, func(a, b Version) int {
 		cmpA := a.Version
