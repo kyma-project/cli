5800edc4298214119843e74e2e691e4d794b8aa1
Cortey <andrzej.pankowski@sap.com>
2024-08-02 15:20:23 +0200

    tests_wip

---
+++ internal/kube/rootlessdynamic/client_test.go
@@ -0,0 +1,22 @@
+package rootlessdynamic
+
+//func Test_Apply(t *testing.T) {
+//	t.Run("Success", func(t *testing.T) {
+//		ctx := context.Background()
+//		client := NewClient()
+//		obj := &unstructured.Unstructured{
+//			Object: map[string]interface{}{
+//				"apiVersion": "v1",
+//				"kind":       "Secret",
+//				"metadata": map[string]interface{}{
+//					"name":      "test",
+//					"namespace": "kyma-system",
+//				},
+//				"data": map[string]interface{}{
+//					"key": "value",
+//				},
+//			},
+//		}
+//
+//	})
+//}
