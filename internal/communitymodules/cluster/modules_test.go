b890f4da02d84323f125094a9d169768b8e84590
Filip Strozik <filip.strozik@outlook.com>
2024-08-22 09:54:33 +0200

    Download module manifest and cr manually from GitHub url (#2210)

--- internal/communitymodules/cluster/modules_test.go
+++ internal/communitymodules/cluster/modules_test.go
@@ -2,7 +2,11 @@ package cluster
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/kyma-project/cli.v3/internal/clierror"
@@ -30,74 +34,129 @@ var (
 	fakeServerlessCR = unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "fakegroup/v1",
-			"kind":       "istio",
+			"kind":       "serverless",
 			"metadata": map[string]interface{}{
-				"name":      "fake-istio",
+				"name":      "fake-serverless",
 				"namespace": "kyma-system",
 			},
 		},
 	}
 
-	fakeAvailableModules = communitymodules.Modules{
+	fakeModuleDetails = []moduleDetails{
 		{
-			Name: "serverless",
-			Versions: []communitymodules.Version{
+			name:    "serverless",
+			version: "1.0.0",
+			cr:      fakeServerlessCR,
+			resources: []unstructured.Unstructured{
 				{
-					Version: "0.0.2",
+					// empty
 				},
 				{
-					Version: "0.0.1",
-					CR:      fakeIstioCR.Object,
-					Resources: []communitymodules.Resource{
-						{
-							//empty
-						},
-						{
-							//empty
-						},
-					},
+					// empty
+				},
+				{
+					// empty
 				},
 			},
 		},
 		{
-			Name: "eventing",
-			Versions: []communitymodules.Version{
+			name:    "istio",
+			version: "0.1.0",
+			cr:      fakeIstioCR,
+			resources: []unstructured.Unstructured{
 				{
-					Version: "1.0.0",
+					// empty
 				},
-			},
-		},
-		{
-			Name: "istio",
-			Versions: []communitymodules.Version{
 				{
-					Version: "0.1.0",
-					CR:      fakeServerlessCR.Object,
-					Resources: []communitymodules.Resource{
-						{
-							//empty
-						},
-						{
-							//empty
-						},
-						{
-							//empty
-						},
-					},
+					// empty
 				},
 			},
 		},
 	}
 )
 
+func Test_downloadSpecifiedModules(t *testing.T) {
+	t.Run("Download module details", func(t *testing.T) {
+		mux := &http.ServeMux{}
+		mux.HandleFunc("/istio/cr", func(w http.ResponseWriter, _ *http.Request) {
+			bytes, err := json.Marshal(fakeIstioCR.Object)
+			require.NoError(t, err)
+
+			_, err = w.Write(bytes)
+			require.NoError(t, err)
+		})
+		mux.HandleFunc("/istio/resources", func(w http.ResponseWriter, _ *http.Request) {
+			data := "\n---\n---\n---\n" // three resources
+
+			_, err := w.Write([]byte(data))
+			require.NoError(t, err)
+		})
+
+		server := httptest.NewServer(mux)
+		defer server.Close()
+
+		availableModules := fixFakeAvailableDetails(
+			fmt.Sprintf("%s/istio/cr", server.URL),
+			fmt.Sprintf("%s/istio/resources", server.URL),
+		)
+
+		modules, err := downloadSpecifiedModules([]ModuleInfo{{"istio", "0.0.1"}}, availableModules)
+		require.Nil(t, err)
+		require.Len(t, modules, 1)
+		require.Equal(t, fakeIstioCR, modules[0].cr)
+		require.Len(t, modules[0].resources, 3)
+	})
+
+	t.Run("broken cr url", func(t *testing.T) {
+		availableModules := fixFakeAvailableDetails(
+			"does-not-exist",
+			"does-not-exist",
+		)
+
+		module, err := downloadSpecifiedModules([]ModuleInfo{{"istio", "0.0.1"}}, availableModules)
+		require.Equal(t, clierror.Wrap(
+			errors.New("Get \"does-not-exist\": unsupported protocol scheme \"\""),
+			clierror.New("failed to download cr from 'does-not-exist' url"),
+		), err)
+		require.Empty(t, module)
+	})
+
+	t.Run("bad response from resources url", func(t *testing.T) {
+		mux := &http.ServeMux{}
+		mux.HandleFunc("/istio/cr", func(w http.ResponseWriter, _ *http.Request) {
+			bytes, err := json.Marshal(fakeIstioCR.Object)
+			require.NoError(t, err)
+
+			_, err = w.Write(bytes)
+			require.NoError(t, err)
+		})
+		mux.HandleFunc("/istio/resources", func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(404)
+		})
+
+		server := httptest.NewServer(mux)
+		defer server.Close()
+
+		availableModules := fixFakeAvailableDetails(
+			fmt.Sprintf("%s/istio/cr", server.URL),
+			fmt.Sprintf("%s/istio/resources", server.URL),
+		)
+
+		modules, err := downloadSpecifiedModules([]ModuleInfo{{"istio", "0.0.1"}}, availableModules)
+		require.Equal(t, clierror.Wrap(
+			errors.New("unexpected status code '404'"),
+			clierror.New(fmt.Sprintf("failed to download manifest from '%s/istio/resources' url", server.URL)),
+		), err)
+		require.Empty(t, modules)
+	})
+}
+
 func Test_applySpecifiedModules(t *testing.T) {
 	t.Run("Apply fake serverless and istio modules", func(t *testing.T) {
 		fakerootlessdynamic := &rootlessdynamicMock{}
 
-		err := applySpecifiedModules(context.Background(), fakerootlessdynamic, []ModuleInfo{
-			{"serverless", "0.0.1"},
-			{"istio", "0.1.0"},
-		}, []unstructured.Unstructured{}, fakeAvailableModules)
+		err := applySpecifiedModules(context.Background(),
+			fakerootlessdynamic, fakeModuleDetails, []unstructured.Unstructured{})
 		require.Nil(t, err)
 
 		require.Len(t, fakerootlessdynamic.appliedObjects, 7)
@@ -111,15 +170,15 @@ func Test_applySpecifiedModules(t *testing.T) {
 		istioCR := fakeIstioCR
 		istioCR.Object["spec"] = "test"
 
-		err := applySpecifiedModules(context.Background(), fakerootlessdynamic, []ModuleInfo{
-			{Name: "istio"},
-		}, []unstructured.Unstructured{
-			istioCR,
-		}, fakeAvailableModules)
+		err := applySpecifiedModules(context.Background(),
+			fakerootlessdynamic, fakeModuleDetails, []unstructured.Unstructured{
+				istioCR,
+			})
 		require.Nil(t, err)
 
-		require.Len(t, fakerootlessdynamic.appliedObjects, 4)
+		require.Len(t, fakerootlessdynamic.appliedObjects, 7)
 		require.Contains(t, fakerootlessdynamic.appliedObjects, istioCR)
+		require.Contains(t, fakerootlessdynamic.appliedObjects, fakeServerlessCR)
 	})
 
 	t.Run("Apply client error", func(t *testing.T) {
@@ -127,9 +186,8 @@ func Test_applySpecifiedModules(t *testing.T) {
 			returnErr: clierror.New("test error"),
 		}
 
-		err := applySpecifiedModules(context.Background(), fakerootlessdynamic, []ModuleInfo{
-			{"serverless", "0.0.1"},
-		}, []unstructured.Unstructured{}, fakeAvailableModules)
+		err := applySpecifiedModules(context.Background(),
+			fakerootlessdynamic, fakeModuleDetails, []unstructured.Unstructured{})
 		require.Equal(t, clierror.Wrap(
 			errors.New("test error"),
 			clierror.New("failed to apply module resources"),
@@ -157,15 +215,12 @@ func fixFakeRootlessDynamicClient() rootlessdynamic.Interface {
 
 func Test_verifyVersion(t *testing.T) {
 	t.Run("Version found", func(t *testing.T) {
-		rec := communitymodules.Module{
-			Name: "test",
-			Versions: []communitymodules.Version{
-				{
-					Version: "1.0.0",
-				},
-				{
-					Version: "1.0.1",
-				},
+		versions := []communitymodules.Version{
+			{
+				Version: "1.0.0",
+			},
+			{
+				Version: "1.0.1",
 			},
 		}
 		moduleInfo := ModuleInfo{
@@ -173,19 +228,16 @@ func Test_verifyVersion(t *testing.T) {
 			Version: "1.0.0",
 		}
 
-		got := verifyVersion(moduleInfo, rec)
-		require.Equal(t, got, rec.Versions[0])
+		got := getDesiredVersion(moduleInfo, versions)
+		require.Equal(t, got, versions[0])
 	})
 	t.Run("Version not found", func(t *testing.T) {
-		rec := communitymodules.Module{
-			Name: "test",
-			Versions: []communitymodules.Version{
-				{
-					Version: "1.0.0",
-				},
-				{
-					Version: "1.0.1",
-				},
+		versions := []communitymodules.Version{
+			{
+				Version: "1.0.0",
+			},
+			{
+				Version: "1.0.1",
 			},
 		}
 		moduleInfo := ModuleInfo{
@@ -193,8 +245,8 @@ func Test_verifyVersion(t *testing.T) {
 			Version: "1.0.2",
 		}
 
-		got := verifyVersion(moduleInfo, rec)
-		require.Equal(t, got, rec.Versions[1])
+		got := getDesiredVersion(moduleInfo, versions)
+		require.Equal(t, got, versions[1])
 	})
 }
 
@@ -223,6 +275,40 @@ func Test_containsModule(t *testing.T) {
 			t.Errorf("containsModule() got = %v, want %v", got, nil)
 		}
 	})
+}
+
+func fixFakeAvailableDetails(istioCRURL, istioResourcesURL string) communitymodules.Modules {
+	return communitymodules.Modules{
+		{
+			Name: "serverless",
+			Versions: []communitymodules.Version{
+				{
+					Version: "0.0.1",
+				},
+			},
+		},
+		{
+			Name: "istio",
+			Versions: []communitymodules.Version{
+				{
+					Version: "0.0.2",
+				},
+				{
+					Version:        "0.0.1",
+					DeploymentYaml: istioResourcesURL,
+					CrYaml:         istioCRURL,
+				},
+			},
+		},
+		{
+			Name: "eventing",
+			Versions: []communitymodules.Version{
+				{
+					Version: "1.0.0",
+				},
+			},
+		},
+	}
 }
 
 type rootlessdynamicMock struct {
