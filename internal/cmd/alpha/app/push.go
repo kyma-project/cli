c0233f4eae9442e07387bc58af82d9cc9bb54b6f
Piotr Halama <piotr.halama@sap.com>
2024-11-14 10:31:02 +0100

    add tests

--- internal/cmd/alpha/app/push.go
+++ internal/cmd/alpha/app/push.go
@@ -1,16 +1,10 @@
 package app
 
 import (
-	"context"
-
 	"github.com/kyma-project/cli.v3/internal/clierror"
 	"github.com/kyma-project/cli.v3/internal/cmdcommon"
-	"github.com/kyma-project/cli.v3/internal/kube"
+	"github.com/kyma-project/cli.v3/internal/kube/resources"
 	"github.com/spf13/cobra"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type appPushConfig struct {
@@ -47,66 +41,14 @@ func NewAppPushCMD(kymaConfig *cmdcommon.KymaConfig) *cobra.Command {
 }
 
 func runAppPush(cfg *appPushConfig) clierror.Error {
-	client, err := cfg.GetKubeClientWithClierr()
-	if err != nil {
-		return err
+	client, clierr := cfg.GetKubeClientWithClierr()
+	if clierr != nil {
+		return clierr
 	}
-	createDeployment(cfg.Ctx, client, cfg.name, cfg.namespace, cfg.image)
+	err := resources.CreateDeployment(cfg.Ctx, client, cfg.name, cfg.namespace, cfg.image)
+	if err != nil {
+		return clierror.Wrap(err, clierror.New("failed to create deployment"))
+	}
 
 	return nil
-}
-
-func createDeployment(ctx context.Context, client kube.Client, name, namespace, image string) {
-	deployment := prepareDeployment(name, image)
-
-	_, err := client.Static().AppsV1().Deployments(namespace).Create(ctx, deployment, metav1.CreateOptions{})
-	if err != nil {
-		panic(err)
-	}
-}
-
-func prepareDeployment(name, image string) *appsv1.Deployment {
-	return &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-			Labels: map[string]string{
-				"app.kubernetes.io/name":        name,
-				"app.kubernetes.io/created-by:": "kyma-cli",
-			},
-		},
-		Spec: appsv1.DeploymentSpec{
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": name,
-				},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: name,
-					Labels: map[string]string{
-						"app":                     name,
-						"sidecar.istio.io/inject": "false",
-					},
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  name,
-							Image: image,
-							Resources: corev1.ResourceRequirements{
-								Requests: corev1.ResourceList{
-									corev1.ResourceMemory: resource.MustParse("64Mi"),
-									corev1.ResourceCPU:    resource.MustParse("50m"),
-								},
-								Limits: corev1.ResourceList{
-									corev1.ResourceMemory: resource.MustParse("128Mi"),
-									corev1.ResourceCPU:    resource.MustParse("100m"),
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
 }
