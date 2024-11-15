1c1ef1b4d7f301e2a3170e30c757288b57a22940
Piotr Halama <piotr.halama@sap.com>
2024-11-15 11:31:25 +0100

    tests

---
+++ internal/types/nullableBool_test.go
@@ -0,0 +1,61 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/kyma-project/cli.v3/internal/types"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	trueConst  = true
+	falseConst = false
+)
+
+func TestNullableBool_Set(t *testing.T) {
+	tests := []struct {
+		name          string
+		value         string
+		expected      *bool
+		expectedError bool
+	}{
+		{
+			name:          "empty",
+			value:         "",
+			expected:      nil,
+			expectedError: false,
+		},
+		{
+			name:          "true",
+			value:         "true",
+			expected:      &trueConst,
+			expectedError: false,
+		},
+		{
+			name:          "false",
+			value:         "false",
+			expected:      &falseConst,
+			expectedError: false,
+		},
+		{
+			name:          "incorrect",
+			value:         "incorrect",
+			expected:      nil,
+			expectedError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nb := types.NullableBool{}
+			err := nb.Set(tt.value)
+			if tt.expectedError {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tt.expected, nb.Value)
+		})
+	}
+
+}
