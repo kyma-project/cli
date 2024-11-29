6cf083b985e7337a22864da600cab970f3c789f2
Filip Strozik <filip.strozik@outlook.com>
2024-11-29 09:17:50 +0100

    Remove `alpha modules` run execution (#2266)

--- internal/cmd/alpha/modules/modules.go
+++ internal/cmd/alpha/modules/modules.go
@@ -1,23 +1,15 @@
 package modules
 
 import (
-	"github.com/kyma-project/cli.v3/internal/clierror"
 	"github.com/kyma-project/cli.v3/internal/cmdcommon"
 	"github.com/spf13/cobra"
 )
 
 func NewModulesCMD(kymaConfig *cmdcommon.KymaConfig) *cobra.Command {
-	cfg := modulesConfig{
-		KymaConfig: kymaConfig,
-	}
-
 	cmd := &cobra.Command{
 		Use:   "modules",
 		Short: "Manage kyma modules.",
 		Long:  `Use this command to manage modules on a kyma cluster.`,
-		Run: func(_ *cobra.Command, _ []string) {
-			clierror.Check(listModules(&cfg))
-		},
 	}
 
 	cmd.AddCommand(NewListCMD(kymaConfig))
