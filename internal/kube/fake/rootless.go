7788980bec66b0e516825332082c07938aa5e95a
Piotr Halama <piotr.halama@sap.com>
2024-12-09 14:47:47 +0100

    rename RootlessDynamicMock

--- internal/kube/fake/rootless.go
+++ internal/kube/fake/rootless.go
@@ -6,28 +6,28 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-type RootlessdynamicMock struct {
+type RootlessDynamicMock struct {
 	ReturnErr      error
 	AppliedObjects []unstructured.Unstructured
 }
 
-func (m *RootlessdynamicMock) Apply(_ context.Context, obj *unstructured.Unstructured) error {
+func (m *RootlessDynamicMock) Apply(_ context.Context, obj *unstructured.Unstructured) error {
 	m.AppliedObjects = append(m.AppliedObjects, *obj)
 	return m.ReturnErr
 }
 
-func (m *RootlessdynamicMock) ApplyMany(_ context.Context, objs []unstructured.Unstructured) error {
+func (m *RootlessDynamicMock) ApplyMany(_ context.Context, objs []unstructured.Unstructured) error {
 	return m.ReturnErr
 }
 
-func (m *RootlessdynamicMock) Get(_ context.Context, obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+func (m *RootlessDynamicMock) Get(_ context.Context, obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
 	return obj, m.ReturnErr
 }
 
-func (m *RootlessdynamicMock) Remove(_ context.Context, obj *unstructured.Unstructured) error {
+func (m *RootlessDynamicMock) Remove(_ context.Context, obj *unstructured.Unstructured) error {
 	return m.ReturnErr
 }
 
-func (m *RootlessdynamicMock) RemoveMany(_ context.Context, objs []unstructured.Unstructured) error {
+func (m *RootlessDynamicMock) RemoveMany(_ context.Context, objs []unstructured.Unstructured) error {
 	return m.ReturnErr
 }
