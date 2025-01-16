a884efd98aef453a869ab3870214bb77b616a9bc
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2025-01-16 10:54:45 +0100

    Implement Acceptance e2e tests for BTP user journey

--- tests/btp/integration-test-btp.sh
+++ tests/btp/integration-test-btp.sh
@@ -18,6 +18,22 @@ echo "Step2: List modules"
 
 # -------------------------------------------------------------------------------------
 echo "Step3: Connecting to remote BTP subaccount"
+
+kubectl create secret generic remote-service-manager-credentials --namespace kyma-system --from-env-file sm.env
+echo "Waiting for CRD btp operator"
+while ! kubectl get crd btpoperators.operator.kyma-project.io; do echo "Waiting for CRD btp operator..."; sleep 1; done
+kubectl wait --for condition=established crd/btpoperators.operator.kyma-project.io
+while ! kubectl get btpoperators.operator.kyma-project.io btpoperator --namespace kyma-system; do echo "Waiting for btpoperator..."; sleep 1; done
+kubectl wait --for condition=Ready btpoperators.operator.kyma-project.io/btpoperator -n kyma-system --timeout=180s
+kyma alpha reference-instance \
+    --btp-secret-name remote-service-manager-credentials \
+    --namespace kyma-system \
+    --offering-name objectstore \
+    --plan-selector standard \
+    --reference-name object-store-reference
+kubectl apply -n kyma-system -f k8s-resources/dependencies/object-store-binding.yaml
+while ! kubectl get secret object-store-reference-binding --namespace kyma-system; do echo "Waiting for object-store-reference-binding secret..."; sleep 5; done
+
 # -------------------------------------------------------------------------------------
 echo "Step4: Create Shared Service Instance Reference"
 # -------------------------------------------------------------------------------------
