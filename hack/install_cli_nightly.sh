087e156bfa6052715f0511e48f45b05758e8b9e4
pPrecel <filip.strozik@outlook.com>
2025-02-24 09:07:35 +0100

    add installation script

---
+++ hack/install_cli_nightly.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+cd ${TMPDIR}
+
+CLI_TMPDIR=${TMPDIR}cli-$(date "+%Y-%m-%d_%H:%M:%S")
+
+echo "creating tmp dir..."
+mkdir ${CLI_TMPDIR}
+cd ${CLI_TMPDIR}
+
+echo "downloading nightly release..."
+curl -sL "https://github.com/kyma-project/cli/releases/download/v0.0.0-dev/kyma_$(uname -s)_$(uname -m).tar.gz" -o ${CLI_TMPDIR}/cli.tar.gz
+
+echo "untaring..."
+tar -zxvf ${CLI_TMPDIR}/cli.tar.gz kyma
+
+set +e
+
+echo "moving kyma binary to the /usr/local/bin directory..."
+cp ${CLI_TMPDIR}/kyma /usr/local/bin/kyma@v3
+
+if [ $? -gt 0 ]; then
+    set -e
+
+    echo "failed to copy, trying with sudo..."
+    sudo cp ${CLI_TMPDIR}/kyma /usr/local/bin/kyma@v3
+fi
+
+echo "removing tmp dir..."
+rm -r ${CLI_TMPDIR}
+
+echo "done"
