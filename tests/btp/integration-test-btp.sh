78da1663677ff1506c144e964af751a42d283249
kwiatekus <krzysztof.kwiatosz@sap.com>
2025-03-12 08:39:06 +0100

    fetch sm creds via tf

--- tests/btp/integration-test-btp.sh
+++ tests/btp/integration-test-btp.sh
@@ -18,10 +18,14 @@ echo "Step2: List modules"
 
 # -------------------------------------------------------------------------------------
 echo "Step3: Connecting to a service manager from remote BTP subaccount"
-# fetch cred.json for remote SM access vie tf resource 
 
-# TODO replace with https://help.sap.com/docs/btp/sap-business-technology-platform/namespace-level-mapping?locale=en-US
-kubectl create secret generic remote-service-manager-credentials --namespace kyma-system --from-env-file sm.env
+# fetch SM binding (cred.json) via terraform  
+terraform -chdir=tf init
+terraform -chdir=tf apply -var-file=.tfvars --auto-approve 
+
+# https://help.sap.com/docs/btp/sap-business-technology-platform/namespace-level-mapping?locale=en-US
+( cd tf ; curl https://raw.githubusercontent.com/kyma-project/btp-manager/main/hack/create-secret-file.sh | bash -s operator remote-service-manager-credentials )
+kubectl create -f tf/btp-access-credentials-secret.yaml || true
 
 # -------------------------------------------------------------------------------------
 echo "Step4: Create service instance reference to a shared object-store service instance"
@@ -34,7 +38,6 @@ kubectl wait --for condition=Ready btpoperators.operator.kyma-project.io/btpoper
 
 
 # TODO - change after btp operator commands are extracted as btp module cli extension
-# Do not pass secret (benefit from namespace-level mapping for kyma-system)
 ../../bin/kyma@v3 alpha reference-instance \
     --btp-secret-name remote-service-manager-credentials \
     --namespace kyma-system \
@@ -64,12 +67,12 @@ dr_internal_pull_url=$(kubectl get dockerregistries.operator.kyma-project.io -n 
 echo "Docker Registry enabled \n\t(URLs: $dr_external_url, $dr_internal_pull_url)"
 echo "\t config.json for docker CLI access generated"
 # -------------------------------------------------------------------------------------
-echo "Step6: Map SAP Hana DB instance with Kyma runtime"
+# echo "Step6: Map SAP Hana DB instance with Kyma runtime"
 
-../../bin/kyma@v3 alpha hana map --credentials-path hana-admin-api-binding.json
+# ../../bin/kyma@v3 alpha hana map --credentials-path hana-admin-api-binding.json
 
 # -------------------------------------------------------------------------------------
-echo "Step7: Push bookstore application (w/o Dockerfile)"
+# echo "Step7: Push bookstore application (w/o Dockerfile)"
 
 # build hdi-deploy via pack and push it via docker CLI (external url)
 pack build hdi-deploy:latest -p sample-http-db-nodejs/hdi-deploy -B paketobuildpacks/builder:base
@@ -83,12 +86,12 @@ docker --config . push $dr_external_url/hdi-deploy:latest
 # -------------------------------------------------------------------------------------
 # echo "Cleanup"
 
-# kubectl delete dockerregistries.operator.kyma-project.io -n kyma-system custom-dr
-# ../../bin/kyma@v3 alpha module delete docker-registry
+kubectl delete dockerregistries.operator.kyma-project.io -n kyma-system custom-dr
+../../bin/kyma@v3 alpha module delete docker-registry
 
-# kubectl delete servicebindings.services.cloud.sap.com -n kyma-system object-store-reference-binding
-# kubectl delete serviceinstances.services.cloud.sap.com -n kyma-system object-store-reference
-# kubectl delete secret -n kyma-system remote-service-manager-credentials 
+kubectl delete servicebindings.services.cloud.sap.com -n kyma-system object-store-reference-binding
+kubectl delete serviceinstances.services.cloud.sap.com -n kyma-system object-store-reference
+kubectl delete secret -n kyma-system remote-service-manager-credentials 
 
 # TODO new command ?
 # ../../bin/kyma@v3 alpha hana unmap --credentials-path hana-admin-api-binding.json
