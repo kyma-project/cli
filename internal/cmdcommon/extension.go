1bd76cd42397040fbe7c42bb57912f573cd6f03f
pPrecel <filip.strozik@outlook.com>
2025-03-14 10:48:34 +0100

    rename func

--- internal/cmdcommon/extension.go
+++ internal/cmdcommon/extension.go
@@ -47,7 +47,7 @@ func newExtensionsConfig(warningWriter io.Writer, config *KymaConfig) *KymaExten
 	return extensionsConfig
 }
 
-func AddShowExtensionsErrorFlag(cmd *cobra.Command) {
+func AddExtensionsFlags(cmd *cobra.Command) {
 	// this flag is not operational. it's only to print help description and help cobra with validation
 	_ = cmd.PersistentFlags().Bool("skip-extensions", false, "Skip fetching extensions from the cluster")
 	_ = cmd.PersistentFlags().Bool("show-extensions-error", false, "Prints a possible error when fetching extensions fails")
