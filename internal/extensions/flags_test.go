431c8164473126a0fff4759bbe216665ff2f578b
Filip Strozik <filip.strozik@outlook.com>
2025-04-08 09:06:49 +0200

    Tests for the `internal/extensions/flag.go` file (#2456)

---
+++ internal/extensions/flags_test.go
@@ -0,0 +1,75 @@
+package extensions
+
+import (
+	"testing"
+
+	"github.com/kyma-project/cli.v3/internal/extensions/parameters"
+	"github.com/kyma-project/cli.v3/internal/extensions/types"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_buildFlag(t *testing.T) {
+	t.Run("build string flag", func(t *testing.T) {
+		expectedValue := parameters.NewTyped(parameters.StringCustomType, ".test.path")
+		_ = expectedValue.SetValue("defval")
+		expectedFlag := flag{
+			value:   expectedValue,
+			warning: nil,
+			pflag: &pflag.Flag{
+				Name:      "test-name",
+				Shorthand: "t",
+				Usage:     "test description",
+				Value:     expectedValue,
+				DefValue:  expectedValue.String(),
+			},
+		}
+
+		givenFlag := buildFlag(types.Flag{
+			Type:         parameters.StringCustomType,
+			Name:         "test-name",
+			Description:  "test description",
+			Shorthand:    "t",
+			ConfigPath:   ".test.path",
+			DefaultValue: "defval",
+			Required:     true,
+		})
+		require.Equal(t, expectedFlag, givenFlag)
+	})
+
+	t.Run("build bool flag", func(t *testing.T) {
+		expectedValue := parameters.NewTyped(parameters.BoolCustomType, ".test.path")
+		_ = expectedValue.SetValue("true")
+		expectedFlag := flag{
+			value:   expectedValue,
+			warning: nil,
+			pflag: &pflag.Flag{
+				Name:        "test-name",
+				Shorthand:   "t",
+				Usage:       "test description",
+				Value:       expectedValue,
+				DefValue:    expectedValue.String(),
+				NoOptDefVal: "true",
+			},
+		}
+
+		givenFlag := buildFlag(types.Flag{
+			Type:         parameters.BoolCustomType,
+			Name:         "test-name",
+			Description:  "test description",
+			Shorthand:    "t",
+			ConfigPath:   ".test.path",
+			DefaultValue: "true",
+			Required:     true,
+		})
+		require.Equal(t, expectedFlag, givenFlag)
+	})
+
+	t.Run("build warning", func(t *testing.T) {
+		givenFlag := buildFlag(types.Flag{
+			Type:         parameters.IntCustomType,
+			DefaultValue: "WRONG VALUE",
+		})
+		require.ErrorContains(t, givenFlag.warning, "parsing \"WRONG VALUE\": invalid syntax")
+	})
+}
