f07640e993fc2566fde72cf9d9a8740098a097c1
Antoni Pstraś <antoni.pstras@sap.com>
2025-05-28 17:57:44 +0200

    List Community Modules

--- internal/modules/render_test.go
+++ internal/modules/render_test.go
@@ -8,10 +8,59 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var testModules = []Module{
+	{
+		Name: "keda",
+		Versions: []ModuleVersion{
+			{
+				Repository: "url-3",
+				Version:    "0.1",
+				Channel:    "regular",
+			},
+			{
+				Version: "0.2",
+				Channel: "fast",
+			},
+		},
+		CommunityModule: false,
+	},
+	{
+		Name: "serverless",
+		Versions: []ModuleVersion{
+			{
+				Repository: "url-1",
+				Version:    "0.0.1",
+				Channel:    "fast",
+			},
+			{
+				Repository: "url-2",
+				Version:    "0.0.2",
+			},
+		},
+		CommunityModule: false,
+	},
+	{
+		Name: "cluster-ip",
+		Versions: []ModuleVersion{
+			{
+				Repository: "url-1",
+				Version:    "0.1.1",
+				Channel:    "",
+			},
+			{
+				Repository: "url-2",
+				Version:    "0.1.2",
+			},
+		},
+		CommunityModule: true,
+	},
+}
+
 const (
-	testCatalogTableView = "NAME         AVAILABLE VERSIONS        \n" +
-		"keda         0.1(regular), 0.2(fast)   \n" +
-		"serverless   0.0.1(fast), 0.0.2        \n"
+	testCatalogTableView = "NAME         AVAILABLE VERSIONS        COMMUNITY   \n" +
+		"keda         0.1(regular), 0.2(fast)   false       \n" +
+		"serverless   0.0.1(fast), 0.0.2        false       \n" +
+		"cluster-ip   0.1.1, 0.1.2              true        \n"
 	testInstalledModulesTableView = "NAME         VERSION       CR POLICY         MANAGED   STATUS      \n" +
 		"keda         0.2(fast)     CreateAndDelete   false     Unmanaged   \n" +
 		"serverless   0.0.1(fast)   Ignore            true      Ready       \n"
@@ -21,7 +70,7 @@ func TestRender(t *testing.T) {
 	t.Run("render table from modules catalog", func(t *testing.T) {
 		buffer := bytes.NewBuffer([]byte{})
 
-		err := renderTable(buffer, testModuleList, CatalogTableInfo)
+		err := renderTable(buffer, testModules, CatalogTableInfo)
 		require.NoError(t, err)
 
 		tableViewBytes, err := io.ReadAll(buffer)
