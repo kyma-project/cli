416f4c6de45e97153bfd7eaed5b2cc7eb39c1e73
Antoni Pstraś <antoni.pstras@sap.com>
2025-07-15 12:02:16 +0200

    Added prompt for module deletion

--- internal/cmd/module/delete.go
+++ internal/cmd/module/delete.go
@@ -37,7 +37,7 @@ func newDeleteCMD(kymaConfig *cmdcommon.KymaConfig) *cobra.Command {
 		},
 	}
 
-	cmd.Flags().BoolVar(&cfg.autoApprove, "auto-approve", false, "Automatically approves community module removal")
+	cmd.Flags().BoolVar(&cfg.autoApprove, "auto-approve", false, "Automatically approves module removal")
 	cmd.Flags().BoolVar(&cfg.community, "community", false, "Delete the community module (if set, the operation targets a community module instead of a core module)")
 
 	return cmd
@@ -56,7 +56,7 @@ func runDelete(cfg *deleteConfig) clierror.Error {
 			return clierr
 		}
 		if len(runningResources) > 0 {
-			confirmationPrompt := prompt.NewBool(prepareMessageContent(runningResources), false)
+			confirmationPrompt := prompt.NewBool(prepareCommunityPromptMessage(runningResources), false)
 			confirmation, err := confirmationPrompt.Prompt()
 			if err != nil {
 				return clierror.Wrap(err, clierror.New("failed to prompt for user input", "if error repeats, consider running the command with --auto-approve flag"))
@@ -68,10 +68,22 @@ func runDelete(cfg *deleteConfig) clierror.Error {
 		}
 	}
 
+	if !cfg.community && !cfg.autoApprove {
+		confirmationPrompt := prompt.NewBool(prepareCorePromptMessage(cfg.module), false)
+		confirmation, err := confirmationPrompt.Prompt()
+		if err != nil {
+			return clierror.Wrap(err, clierror.New("failed to prompt for user input", "if error repeats, consider running the command with --auto-approve flag"))
+		}
+
+		if !confirmation {
+			return nil
+		}
+	}
+
 	return modules.Disable(cfg.Ctx, client, moduleTemplatesRepo, cfg.module, cfg.community)
 }
 
-func prepareMessageContent(resourcesNames []string) string {
+func prepareCommunityPromptMessage(resourcesNames []string) string {
 	var buf bytes.Buffer
 
 	fmt.Fprint(&buf, "There are currently associated resources related to this module still running on the cluster:\n")
@@ -80,6 +92,15 @@ func prepareMessageContent(resourcesNames []string) string {
 	}
 	fmt.Fprint(&buf, "\nDeleting the module may affect these resources.\n")
 	fmt.Fprint(&buf, "Are you sure you want to proceed with the deletion?")
+
+	return buf.String()
+}
+
+func prepareCorePromptMessage(moduleName string) string {
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "Are you sure you want to delete module %s?\n", moduleName)
+	fmt.Fprintf(&buf, "Before you delete the %s module, make sure the module resources are no longer needed. This action also permanently removes the namespaces, service instances, and service bindings created by the module.\n", moduleName)
+	fmt.Fprintf(&buf, "Are you sure you want to continue?")
 
 	return buf.String()
 }
