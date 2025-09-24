25257d290c8ccd9114e8e363f6fa735186917404
Antoni Pstraś <antoni.pstras@sap.com>
2025-09-24 14:20:50 +0200

    Extended diagnostics with non-ready modules data

--- internal/diagnostics/diagnose.go
+++ internal/diagnostics/diagnose.go
@@ -3,24 +3,28 @@ package diagnostics
 import (
 	"context"
 	"io"
+	"os"
 
 	"github.com/kyma-project/cli.v3/internal/kube"
 )
 
 type DiagnosticData struct {
-	Metadata      Metadata           `json:"metadata" yaml:"metadata"`
-	Warnings      KymaSystemWarnings `json:"warnings" yaml:"warnings"`
-	NodeResources []NodeResourceInfo `json:"nodes" yaml:"nodes"`
+	Metadata                  Metadata                    `json:"metadata" yaml:"metadata"`
+	Warnings                  KymaSystemWarnings          `json:"warnings" yaml:"warnings"`
+	NodeResources             []NodeResourceInfo          `json:"nodes" yaml:"nodes"`
+	ModuleCustomResourceState []ModuleCustomResourceState `json:"moduleCustomResourceStates" yaml:"moduleCustomResourceStates"`
 }
 
 func GetData(ctx context.Context, client kube.Client, output io.Writer, verbose bool) DiagnosticData {
 	metadataCollector := NewMetadataCollector(client, output, verbose)
 	kymaSystemWarningsCollector := NewKymaSystemWarningsCollector(client, output, verbose)
 	nodeResourceInfoCollector := NewNodeResourceInfoCollector(client, output, verbose)
+	modulesCustomResourceStates := NewModuleCustomResourceStateCollector(client, os.Stdout, verbose)
 
 	return DiagnosticData{
-		Metadata:      metadataCollector.Run(ctx),
-		Warnings:      kymaSystemWarningsCollector.Run(ctx),
-		NodeResources: nodeResourceInfoCollector.Run(ctx),
+		Metadata:                  metadataCollector.Run(ctx),
+		Warnings:                  kymaSystemWarningsCollector.Run(ctx),
+		NodeResources:             nodeResourceInfoCollector.Run(ctx),
+		ModuleCustomResourceState: modulesCustomResourceStates.Run(ctx),
 	}
 }
