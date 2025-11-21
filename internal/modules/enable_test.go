67b17a1b2109995ca23d9b87d31e69eef7719842
grego952 <grzegorz.karaluch@sap.com>
2025-11-21 12:34:55 +0100

    Improve hints

--- internal/modules/enable_test.go
+++ internal/modules/enable_test.go
@@ -110,10 +110,10 @@ func TestEnable(t *testing.T) {
 		repo := &modulesfake.ModuleTemplatesRepo{}
 
 		hints := []string{
-			"make sure you provide valid module name and channel (or version)",
-			"list available modules by calling the `kyma module catalog` command",
-			"pull available modules by calling the `kyma module pull` command",
-			"if you want to add a community module, use the `--origin` flag",
+			"make sure you provide a valid module name and channel (or version)",
+			"to list available modules, call the `kyma module catalog` command",
+			"to pull available modules, call the `kyma module pull` command",
+			"to add a community module, use the `--origin` flag",
 		}
 
 		expectedCliErr := clierror.Wrap(
@@ -140,10 +140,10 @@ func TestEnable(t *testing.T) {
 		repo := &modulesfake.ModuleTemplatesRepo{}
 
 		hints := []string{
-			"make sure you provide valid module name and channel (or version)",
-			"list available modules by calling the `kyma module catalog` command",
-			"pull available modules by calling the `kyma module pull` command",
-			"if you want to add a community module, use the `--origin` flag",
+			"make sure you provide a valid module name and channel (or version)",
+			"to list available modules, call the `kyma module catalog` command",
+			"to pull available modules, call the `kyma module pull` command",
+			"to add a community module, use the `--origin` flag",
 		}
 
 		expectedCliErr := clierror.Wrap(
