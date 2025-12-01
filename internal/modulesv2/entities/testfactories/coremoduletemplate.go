3875e39c64dc7b9320ddeb2cab7fd0fa9b585b02
Antoni Pstraś <antoni.pstras@sap.com>
2025-12-01 09:05:38 +0100

    Added tests to moduletemplates repository

---
+++ internal/modulesv2/entities/testfactories/coremoduletemplate.go
@@ -0,0 +1,46 @@
+package testfactories
+
+import "github.com/kyma-project/cli.v3/internal/modulesv2/entities"
+
+type Params struct {
+	TemplateName string
+	ModuleName   string
+	Version      string
+	Channel      string
+	Namespace    string
+}
+
+func CoreModuleTemplate(params *Params) *entities.CoreModuleTemplate {
+	defaults := defaultParams()
+
+	if params == nil {
+		params = defaults
+	}
+
+	return entities.NewCoreModuleTemplateFromParams(
+		firstNonEmpty(params.TemplateName, defaults.TemplateName),
+		firstNonEmpty(params.ModuleName, defaults.ModuleName),
+		firstNonEmpty(params.Version, defaults.Version),
+		firstNonEmpty(params.Channel, defaults.Channel),
+		firstNonEmpty(params.Namespace, defaults.Namespace),
+	)
+}
+
+func defaultParams() *Params {
+	return &Params{
+		TemplateName: "sample-template-0.0.1",
+		ModuleName:   "sample-template",
+		Version:      "0.0.1",
+		Channel:      "fast",
+		Namespace:    "kyma-system",
+	}
+}
+
+func firstNonEmpty(values ...string) string {
+	for _, v := range values {
+		if v != "" {
+			return v
+		}
+	}
+	return ""
+}
