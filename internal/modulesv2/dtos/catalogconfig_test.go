d044b03468e9c23a9dbe98c03ded529a551c5cfc
Antoni Pstraś <antoni.pstras@sap.com>
2026-01-20 16:03:21 +0100

    Updated flags definition in alpha module catalog

--- internal/modulesv2/dtos/catalogconfig_test.go
+++ internal/modulesv2/dtos/catalogconfig_test.go
@@ -3,44 +3,55 @@ package dtos_test
 import (
 	"testing"
 
-	"github.com/kyma-project/cli.v3/internal/flags"
 	"github.com/kyma-project/cli.v3/internal/modulesv2/dtos"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_NewCatalogConfigFromRemote(t *testing.T) {
 	tests := []struct {
-		remote         flags.BoolOrStrings
+		remote         bool
+		remoteUrl      []string
 		expectedConfig dtos.CatalogConfig
 	}{
 		{
-			remote:         flags.BoolOrStrings{Enabled: false, Values: nil},
+			remote:         false,
+			remoteUrl:      nil,
 			expectedConfig: dtos.CatalogConfig{ListKyma: true, ListCluster: true},
 		},
 		{
-			remote:         flags.BoolOrStrings{Enabled: false, Values: []string{}},
+			remote:         false,
+			remoteUrl:      []string{},
 			expectedConfig: dtos.CatalogConfig{ListKyma: true, ListCluster: true},
 		},
 		{
-			remote:         flags.BoolOrStrings{Enabled: true, Values: nil},
+			remote:         true,
+			remoteUrl:      nil,
 			expectedConfig: dtos.CatalogConfig{ExternalUrls: []string{dtos.KYMA_COMMUNITY_MODULES_REPOSITORY_URL}},
 		},
 		{
-			remote:         flags.BoolOrStrings{Enabled: true, Values: []string{}},
+			remote:         true,
+			remoteUrl:      []string{},
 			expectedConfig: dtos.CatalogConfig{ExternalUrls: []string{dtos.KYMA_COMMUNITY_MODULES_REPOSITORY_URL}},
 		},
 		{
-			remote:         flags.BoolOrStrings{Enabled: true, Values: []string{"https://external-repo.co.uk", "https://example.com"}},
+			remote:         false,
+			remoteUrl:      []string{"https://external-repo.co.uk", "https://example.com"},
 			expectedConfig: dtos.CatalogConfig{ExternalUrls: []string{"https://external-repo.co.uk", "https://example.com"}},
 		},
 		{
-			remote:         flags.BoolOrStrings{Enabled: true, Values: []string{"https://external-repo.co.uk", "https://example.com", "https://external-repo.co.uk", "https://external-repo.co.uk"}},
+			remote:         true,
+			remoteUrl:      []string{"https://external-repo.co.uk", "https://example.com"},
+			expectedConfig: dtos.CatalogConfig{ExternalUrls: []string{dtos.KYMA_COMMUNITY_MODULES_REPOSITORY_URL, "https://external-repo.co.uk", "https://example.com"}},
+		},
+		{
+			remote:         false,
+			remoteUrl:      []string{"https://external-repo.co.uk", "https://example.com", "https://external-repo.co.uk", "https://external-repo.co.uk"},
 			expectedConfig: dtos.CatalogConfig{ExternalUrls: []string{"https://external-repo.co.uk", "https://example.com"}},
 		},
 	}
 
 	for _, test := range tests {
-		result := dtos.NewCatalogConfigFromRemote(test.remote)
+		result := dtos.NewCatalogConfigFromRemote(test.remote, test.remoteUrl)
 
 		require.Equal(t, test.expectedConfig.ListKyma, result.ListKyma)
 		require.Equal(t, test.expectedConfig.ListCluster, result.ListCluster)
