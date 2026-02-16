ee7e191c6aea4c404e6903448bef85b28ff32c2e
Filip Rudy <filip.rudy@sap.com>
2026-02-16 18:43:56 +0100

    fix typo

--- internal/modules/enable.go
+++ internal/modules/enable.go
@@ -63,7 +63,7 @@ func crMsgSuffix(customCount int, defaultApplicable bool) string {
 
 func channelMsgSuffix(channel string) string {
 	if len(channel) > 0 {
-		return ", in channel " + channel
+		return ", from channel " + channel
 	}
 	return ""
 }
