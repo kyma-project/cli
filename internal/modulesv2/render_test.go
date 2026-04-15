20599deddaa4e9d0df5f74c8ae8d78b935964959
Marcin Dobrochowski <anoip@o2.pl>
2026-04-15 21:31:07 +0200

    add yaml output format to list command

--- internal/modulesv2/render_test.go
+++ internal/modulesv2/render_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/kyma-project/cli.v3/internal/modulesv2/dtos"
 	"github.com/kyma-project/cli.v3/internal/out"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestRenderList_Table(t *testing.T) {
@@ -33,4 +34,22 @@ func TestRenderList_JSON(t *testing.T) {
 
 	require.NoError(t, err)
 	require.JSONEq(t, `[{"name":"api-gateway","version":"3.5.1","channel":"regular"}]`, buf.String())
+}
+
+func TestRenderList_YAML(t *testing.T) {
+	results := []dtos.ListResult{
+		{Name: "api-gateway", Version: "3.5.1", Channel: "regular"},
+	}
+
+	var buf bytes.Buffer
+	err := RenderList(results, types.YAMLFormat, out.NewToWriter(&buf))
+
+	require.NoError(t, err)
+	var parsed []map[string]interface{}
+	require.NoError(t, yaml.Unmarshal(buf.Bytes(), &parsed))
+	require.Len(t, parsed, 1)
+	module := parsed[0]
+	require.Equal(t, "api-gateway", module["name"])
+	require.Equal(t, "3.5.1", module["version"])
+	require.Equal(t, "regular", module["channel"])
 }
